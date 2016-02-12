da7292a1d90b7653b2107db559974fd2aed37d64
Esteve Fernandez <esteve@apache.org>
2016-02-12 13:25:15 -0800

    Fix syntax warnings

--- rclpy/publisher.py
+++ rclpy/publisher.py
@@ -1,4 +1,19 @@
-import rclpy._rclpy__rmw_opensplice_cpp as _rclpy
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+
 
 class Publisher:
     def __init__(self, publisher_handle, msg_type, topic, qos_profile):
@@ -8,4 +23,4 @@ class Publisher:
         self.qos_profile = qos_profile
 
     def publish(self, msg):
-        _rclpy.rclpy_publish(self.publisher_handle, msg)
+        rclpy._rclpy.rclpy_publish(self.publisher_handle, msg)
