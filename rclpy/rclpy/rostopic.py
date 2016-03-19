727c7598b071d21265d130919b6b07b163538379
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2016-03-19 20:57:17 +0100

    style fixes

--- rclpy/rclpy/rostopic.py
+++ rclpy/rclpy/rostopic.py
@@ -25,37 +25,149 @@ from optparse import OptionParser
 
 NAME='rostopic'
 
+# TODO implement
 def _rostopic_cmd_hz(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_type(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_list(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_info(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_pub(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_bw(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_find(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
+# TODO implement
 def _rostopic_cmd_delay(argv):
-    pass
+    print("NOT IMPLEMENTED\n")
+    sys.exit(0)
+
 def _rostopic_cmd_echo(argv):
-    pass
+    def expr_eval(expr):
+        def eval_fn(m):
+            return eval(expr)
+        return eval_fn
+
+    args = argv[2:]
+    from optparse import OptionParser
+    parser = OptionParser(usage="usage: %prog echo [options] /topic", prog=NAME)
+    parser.add_option("-b", "--bag",
+                      dest="bag", default=None,
+                      help="echo messages from .bag file", metavar="BAGFILE")
+    parser.add_option("-p", 
+                      dest="plot", default=False,
+                      action="store_true",
+                      help="echo in a plotting friendly format")
+    parser.add_option("-w",
+                      dest="fixed_numeric_width", default=None, metavar="NUM_WIDTH",
+                      help="fixed width for numeric values")
+    parser.add_option("--filter", 
+                      dest="filter_expr", default=None,
+                      metavar="FILTER-EXPRESSION",
+                      help="Python expression to filter messages that are printed. Expression can use Python builtins as well as m (the message) and topic (the topic name).")
+    parser.add_option("--nostr", 
+                      dest="nostr", default=False,
+                      action="store_true",
+                      help="exclude string fields")
+    parser.add_option("--noarr",
+                      dest="noarr", default=False,
+                      action="store_true",
+                      help="exclude arrays")
+    parser.add_option("-c", "--clear",
+                      dest="clear", default=False,
+                      action="store_true",
+                      help="clear screen before printing next message")
+    parser.add_option("-a", "--all",
+                      dest="all_topics", default=False,
+                      action="store_true",
+                      help="display all message in bag, only valid with -b option")
+    parser.add_option("-n", 
+                      dest="msg_count", default=None, metavar="COUNT",
+                      help="number of messages to echo")
+    parser.add_option("--offset",
+                      dest="offset_time", default=False,
+                      action="store_true",
+                      help="display time as offsets from current time (in seconds)")
+
+    (options, args) = parser.parse_args(args)
+    if len(args) > 1:
+        parser.error("you may only specify one input topic")
+    if options.all_topics and not options.bag:
+        parser.error("Display all option is only valid when echoing from bag files")
+    if options.offset_time and options.bag:
+        parser.error("offset time option is not valid with bag files")
+    if options.all_topics:
+        topic = ''
+    else:
+        if len(args) == 0:
+            parser.error("topic must be specified")        
+        topic = rosgraph.names.script_resolve_name('rostopic', args[0])
+        # suppressing output to keep it clean
+        #if not options.plot:
+        #    print "rostopic: topic is [%s]"%topic
+        
+    filter_fn = None
+    if options.filter_expr:
+        filter_fn = expr_eval(options.filter_expr)
+
+    try:
+        msg_count = int(options.msg_count) if options.msg_count else None
+    except ValueError:
+        parser.error("COUNT must be an integer")
+
+    try:
+        fixed_numeric_width = int(options.fixed_numeric_width) if options.fixed_numeric_width else None
+        if fixed_numeric_width is not None and fixed_numeric_width < 2:
+            parser.error("Fixed width for numeric values must be at least 2")
+    except ValueError:
+        parser.error("NUM_WIDTH must be an integer")
+
+    field_filter_fn = create_field_filter(options.nostr, options.noarr)
+    callback_echo = CallbackEcho(topic, None, plot=options.plot,
+                                 filter_fn=filter_fn,
+                                 echo_clear=options.clear, echo_all_topics=options.all_topics,
+                                 offset_time=options.offset_time, count=msg_count,
+                                 field_filter_fn=field_filter_fn, fixed_numeric_width=fixed_numeric_width)
+    try:
+        _rostopic_echo(topic, callback_echo, bag_file=options.bag)
+    except socket.error:
+        sys.stderr.write("Network communication failed. Most likely failed to communicate with master.\n")
 
 def _fullusage():
     print("""rostopic is a command-line tool for printing information about ROS Topics.
 Commands:
-\trostopic bw (NOT IMPLEMENTED)\tdisplay bandwidth used by topic
-\trostopic delay (NOT IMPLEMENTED)\tdisplay delay of topic from timestamp in header
+\trostopic bw\tdisplay bandwidth used by topic
+\trostopic delay\tdisplay delay of topic from timestamp in header
 \trostopic echo\tprint messages to screen
-\trostopic find (NOT IMPLEMENTED)\tfind topics by type
-\trostopic hz (NOT IMPLEMENTED)\tdisplay publishing rate of topic    
-\trostopic info (NOT IMPLEMENTED)\tprint information about active topic
-\trostopic list (NOT IMPLEMENTED)\tlist active topics
-\trostopic pub (NOT IMPLEMENTED)\tpublish data to topic
-\trostopic type (NOT IMPLEMENTED)\tprint topic type
+\trostopic find\tfind topics by type
+\trostopic hz\tdisplay publishing rate of topic    
+\trostopic info\tprint information about active topic
+\trostopic list\tlist active topics
+\trostopic pub\tpublish data to topic
+\trostopic type\tprint topic type
 Type rostopic <command> -h for more detailed usage, e.g. 'rostopic echo -h'
 """)
     sys.exit(getattr(os, 'EX_USAGE', 1))
