10266a0653172273cbbddbd6cd39cc57f1f52de9
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-06-21 12:16:30 -0700

    use class method import_type_support add subscriber/publisher creation

--- rclpy/rclpy/node.py
+++ rclpy/rclpy/node.py
@@ -16,8 +16,6 @@ import rclpy
 
 from rclpy.publisher import Publisher
 from rclpy.subscription import Subscription
-from rosidl_generator_py import import_type_support
-from rosidl_cmake import convert_camel_case_to_lower_case_underscore
 
 
 class Node:
@@ -28,32 +26,16 @@ class Node:
 
     def create_publisher(self, msg_type, topic, qos_profile):
         if msg_type.__class__._TYPE_SUPPORT is None:
-            self._import_type_support(msg_type)
+            msg_type.__class__.__import_type_support__()
         publisher_handle = rclpy._rclpy.rclpy_create_publisher(self.handle, msg_type, topic)
 
         return Publisher(publisher_handle, msg_type, topic, qos_profile)
 
     def create_subscription(self, msg_type, topic, callback, qos_profile):
         if msg_type.__class__._TYPE_SUPPORT is None:
-            self._import_type_support(msg_type)
+            msg_type.__class__.__import_type_support__()
         subscription_handle = rclpy._rclpy.rclpy_create_subscription(self.handle, msg_type, topic)
 
         subscription = Subscription(subscription_handle, msg_type, topic, callback, qos_profile)
         self.subscriptions.append(subscription)
         return subscription
-
-    def _import_type_support(self, msg_type):
-        mod = msg_type.__class__.__module__
-        pkg_name = mod[:mod.find('.')]
-        subfolder = mod[mod.find('.') + 1:mod.rfind('.')]
-        module = import_type_support(
-            pkg_name, subfolder, msg_type,
-            rclpy._rclpy.rclpy_get_rmw_implementation_identifier())
-        lowercase_msg_type = convert_camel_case_to_lower_case_underscore(msg_type.__name__)
-        msg_type.__class__._CONVERT_FROM_PY = module.__dict__.get(
-            'convert_from_py_' + lowercase_msg_type)
-        msg_type.__class__._CONVERT_TO_PY = module.__dict__.get(
-            'convert_to_py_' + lowercase_msg_type)
-        msg_type.__class__._TYPE_SUPPORT = module.__dict__.get(
-            'type_support_' + lowercase_msg_type)
-        # FIXME(mikaelarguedas) what about nested messages ? Should import all these recursively
