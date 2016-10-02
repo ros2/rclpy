e7b33065664c9a89dd02f9d327a8dd2d40b08e7f
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-10-01 19:44:48 -0700

    initialize tnat struct before query

--- rclpy/src/rclpy/_rclpy.c
+++ rclpy/src/rclpy/_rclpy.c
@@ -424,6 +424,9 @@ rclpy_get_topic_names_and_types(PyObject * Py_UNUSED(self), PyObject * args)
   assert(pynode != NULL);
   assert(node != NULL);
   rcl_topic_names_and_types_t topic_names_and_types;
+  topic_names_and_types.topic_count = 0;
+  topic_names_and_types.topic_names = NULL;
+  topic_names_and_types.type_names = NULL;
   rcl_ret_t ret = rcl_get_topic_names_and_types(node, &topic_names_and_types);
   if (ret != RCL_RET_OK) {
     PyErr_Format(PyExc_RuntimeError,
@@ -436,7 +439,6 @@ rclpy_get_topic_names_and_types(PyObject * Py_UNUSED(self), PyObject * args)
   PyObject * pytopic_names_types = NULL;
   pytopic_names_types = PyObject_CallObject(pytopic_names_types_class, NULL);
 
-  fprintf(stderr, "topic_count= %zd\n", topic_names_and_types.topic_count);
   PyObject * pytopic_names = PyList_New(topic_names_and_types.topic_count);
   PyObject * pytype_names = PyList_New(topic_names_and_types.topic_count);
   size_t idx;
