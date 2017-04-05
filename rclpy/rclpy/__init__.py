913698ee392c73114d3a3409f94f162962b314ec
William Woodall <william+github@osrfoundation.org>
2017-04-05 10:55:42 -0700

    make namespace argument a keyword-only argument

--- rclpy/rclpy/__init__.py
+++ rclpy/rclpy/__init__.py
@@ -35,7 +35,7 @@ def init(args=None):
     return _rclpy.rclpy_init(args if args is not None else sys.argv)
 
 
-def create_node(node_name, namespace=None):
+def create_node(node_name, *, namespace=None):
     node_handle = _rclpy.rclpy_create_node(node_name, namespace or '')
     return Node(node_handle)
 
