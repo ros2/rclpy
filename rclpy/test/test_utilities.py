655425fabcdd20cba75ad051ee00289fc1f3cb4c
Shane Loretz <sloretz@osrfoundation.org>
2017-10-17 08:48:06 -0700

    Use S_TO_NS in test

--- rclpy/test/test_utilities.py
+++ rclpy/test/test_utilities.py
@@ -14,6 +14,7 @@
 
 import unittest
 
+from rclpy.constants import S_TO_NS
 import rclpy.utilities
 
 
@@ -23,7 +24,7 @@ class TestUtilities(unittest.TestCase):
         self.assertGreater(0, rclpy.utilities.timeout_sec_to_nsec(None))
         self.assertGreater(0, rclpy.utilities.timeout_sec_to_nsec(-1))
         self.assertEqual(0, rclpy.utilities.timeout_sec_to_nsec(0))
-        self.assertEqual(1000000000, rclpy.utilities.timeout_sec_to_nsec(1))
+        self.assertEqual(1.5 * S_TO_NS, rclpy.utilities.timeout_sec_to_nsec(1.5))
 
 
 if __name__ == '__main__':
