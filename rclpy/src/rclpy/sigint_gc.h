ae10656497ed7a530eb06c3ba20f4ab44bd1f5ee
Shane Loretz <sloretz@osrfoundation.org>
2017-11-09 14:37:43 -0800

    rclpy_sigint dllimport/dllexport

--- rclpy/src/rclpy/sigint_gc.h
+++ rclpy/src/rclpy/sigint_gc.h
@@ -18,10 +18,42 @@
 
 #include <rcl/rcl.h>
 
+// This logic was borrowed (then namespaced) from the examples on the gcc wiki:
+//     https://gcc.gnu.org/wiki/Visibility
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define RCLPY_SIGINT_EXPORT __attribute__ ((dllexport))
+    #define RCLPY_SIGINT_IMPORT __attribute__ ((dllimport))
+  #else
+    #define RCLPY_SIGINT_EXPORT __declspec(dllexport)
+    #define RCLPY_SIGINT_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef RCLPY_SIGINT_BUILDING_LIBRARY
+    #define RCLPY_SIGINT_PUBLIC RCLPY_SIGINT_EXPORT
+  #else
+    #define RCLPY_SIGINT_PUBLIC RCLPY_SIGINT_IMPORT
+  #endif
+  #define RCLPY_SIGINT_PUBLIC_TYPE RCLPY_SIGINT_PUBLIC
+  #define RCLPY_SIGINT_LOCAL
+#else
+  #define RCLPY_SIGINT_EXPORT __attribute__ ((visibility("default")))
+  #define RCLPY_SIGINT_IMPORT
+  #if __GNUC__ >= 4
+    #define RCLPY_SIGINT_PUBLIC __attribute__ ((visibility("default")))
+    #define RCLPY_SIGINT_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define RCLPY_SIGINT_PUBLIC
+    #define RCLPY_SIGINT_LOCAL
+  #endif
+  #define RCLPY_SIGINT_PUBLIC_TYPE
+#endif
+
+
 /// Global variable with guard condition for sigint handler
-extern rcl_guard_condition_t * g_rclpy_sigint_gc_handle;
+extern RCLPY_SIGINT_PUBLIC rcl_guard_condition_t * g_rclpy_sigint_gc_handle;
 
 /// Function that can be used as a sigint handler
-extern void rclpy_catch_function(int signo);
+extern RCLPY_SIGINT_PUBLIC void rclpy_catch_function(int signo);
 
 #endif  // RCLPY__SIGINT_GC_H_
