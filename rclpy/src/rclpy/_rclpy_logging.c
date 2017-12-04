f4fb864ba437621b621290eb361e02e5a5e4e36d
dhood <dhood@users.noreply.github.com>
2017-12-03 18:40:22 -0800

    Rename severity_thresold -> level (#165)
    * Rename severity_thresold -> level
    
    * Fix strange variable naming

--- rclpy/src/rclpy/_rclpy_logging.c
+++ rclpy/src/rclpy/_rclpy_logging.c
@@ -55,61 +55,61 @@ rclpy_logging_shutdown(PyObject * Py_UNUSED(self), PyObject * Py_UNUSED(args))
   Py_RETURN_NONE;
 }
 
-/// Set the severity threshold of a logger.
+/// Set the level of a logger.
 /**
  *
  * \param[in] name Fully-qualified name of logger.
- * \param[in] severity Threshold to set
+ * \param[in] level to set
  * \return None or
  * \return NULL on failure
  */
 static PyObject *
-rclpy_logging_set_logger_severity_threshold(PyObject * Py_UNUSED(self), PyObject * args)
+rclpy_logging_set_logger_level(PyObject * Py_UNUSED(self), PyObject * args)
 {
   const char * name;
-  int severity;
-  if (!PyArg_ParseTuple(args, "si", &name, &severity)) {
+  int level;
+  if (!PyArg_ParseTuple(args, "si", &name, &level)) {
     return NULL;
   }
 
-  rcutils_ret_t ret = rcutils_logging_set_logger_severity_threshold(name, severity);
+  rcutils_ret_t ret = rcutils_logging_set_logger_level(name, level);
   if (ret != RCUTILS_RET_OK) {
     PyErr_Format(PyExc_RuntimeError,
-      "Failed to set severity threshold \"%d\" for logger \"%s\", return code: %d\n",
-      severity, name, ret);
+      "Failed to set level \"%d\" for logger \"%s\", return code: %d\n",
+      level, name, ret);
     rcutils_reset_error();
     return NULL;
   }
   Py_RETURN_NONE;
 }
 
-/// Get the effective severity threshold of a logger.
+/// Get the effective level of a logger.
 /**
- * The effective severity threshold is determined as the logger severity if it has been set,
- * otherwise it defers to the severity threshold of the logger's ancestors, and if all are unset
- * the default severity threshold is used.
+ * The "effective" logger level is determined as the logger level if it has been set explicitly,
+ * otherwise it defers to the level of the logger's ancestors, and if all are unset
+ * the default level is used.
  *
  * \param[in] name Fully-qualified name of logger.
- * \return The effective severity threshold, or
+ * \return The effective level, or
  * \return NULL of failure.
  */
 static PyObject *
-rclpy_logging_get_logger_effective_severity_threshold(PyObject * Py_UNUSED(self), PyObject * args)
+rclpy_logging_get_logger_effective_level(PyObject * Py_UNUSED(self), PyObject * args)
 {
   const char * name;
   if (!PyArg_ParseTuple(args, "s", &name)) {
     return NULL;
   }
-  int severity = rcutils_logging_get_logger_effective_severity_threshold(name);
+  int logger_level = rcutils_logging_get_logger_effective_level(name);
 
-  if (severity < 0) {
+  if (logger_level < 0) {
     PyErr_Format(PyExc_RuntimeError,
-      "Failed to get effective severity threshold for logger \"%s\", return code: %d\n",
-      name, severity);
+      "Failed to get effective level for logger \"%s\", return code: %d\n",
+      name, logger_level);
     rcutils_reset_error();
     return NULL;
   }
-  return PyLong_FromLong(severity);
+  return PyLong_FromLong(logger_level);
 }
 
 /// Determine if the logger is enabled for a severity.
@@ -180,13 +180,13 @@ static PyMethodDef rclpy_logging_methods[] = {
     "Shutdown the logging system."
   },
   {
-    "rclpy_logging_set_logger_severity_threshold", rclpy_logging_set_logger_severity_threshold,
-    METH_VARARGS, "Set the severity threshold of a logger."
+    "rclpy_logging_set_logger_level", rclpy_logging_set_logger_level,
+    METH_VARARGS, "Set the level of a logger."
   },
   {
-    "rclpy_logging_get_logger_effective_severity_threshold",
-    rclpy_logging_get_logger_effective_severity_threshold,
-    METH_VARARGS, "Get the effective severity threshold of a logger."
+    "rclpy_logging_get_logger_effective_level",
+    rclpy_logging_get_logger_effective_level,
+    METH_VARARGS, "Get the effective level of a logger."
   },
   {
     "rclpy_logging_logger_is_enabled_for", rclpy_logging_logger_is_enabled_for,
