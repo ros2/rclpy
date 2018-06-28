3b993a8d139a56fc83ab36e402e5ce2a2eaf316e
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-06-28 15:18:45 -0700

    Py_DECREF entity_ready_list

--- rclpy/src/rclpy/_rclpy.c
+++ rclpy/src/rclpy/_rclpy.c
@@ -2327,6 +2327,7 @@ rclpy_get_ready_entities(PyObject * Py_UNUSED(self), PyObject * args)
   } else if (0 == strcmp(entity_type, "guard_condition")) {
     GET_LIST_READY_ENTITIES(guard_condition)
   } else {
+    Py_DECREF(entity_ready_list);
     PyErr_Format(PyExc_RuntimeError,
       "'%s' is not a known entity", entity_type);
     return NULL;
