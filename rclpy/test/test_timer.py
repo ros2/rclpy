88ff1a21f2cef0f8390c4f07e8d7d2d738b6039e
Mikael Arguedas <mikael@osrfoundation.org>
2018-08-14 18:20:32 -0700

    Revert "disable 1kHz test on all platforms"
    This reverts commit 758a74f8bddbc57a73b6b212291c5095f0c71faa.

--- rclpy/test/test_timer.py
+++ rclpy/test/test_timer.py
@@ -13,9 +13,8 @@
 # limitations under the License.
 
 import multiprocessing
-# TODO(mikaelarguedas) comment back in if we reenable 1kHz test for some platforms
-# import os
-# import platform
+import os
+import platform
 import sys
 import time
 import traceback
@@ -154,10 +153,9 @@ def test_timer_zero_callbacks100hertz():
 # on every platform at high frequency
 # TODO(sloretz) Reenable on arm when executor performance is good enough
 def test_timer_zero_callbacks1000hertz():
-    # if os.name == 'nt' or platform.machine() == 'aarch64':
-    #     raise SkipTest
-    # func_launch(func_zero_callback, ['0.001'], 'received callbacks when not expected')
-    raise SkipTest
+    if os.name == 'nt' or platform.machine() == 'aarch64':
+        raise SkipTest
+    func_launch(func_zero_callback, ['0.001'], 'received callbacks when not expected')
 
 
 def test_timer_number_callbacks10hertz():
@@ -170,11 +168,10 @@ def test_timer_number_callbacks100hertz():
 
 
 def test_timer_number_callbacks1000hertz():
-    # if os.name == 'nt' or platform.machine() == 'aarch64':
-    #     raise SkipTest
-    # func_launch(
-    #     func_number_callbacks, ['0.001'], "didn't receive the expected number of callbacks")
-    raise SkipTest
+    if os.name == 'nt' or platform.machine() == 'aarch64':
+        raise SkipTest
+    func_launch(
+        func_number_callbacks, ['0.001'], "didn't receive the expected number of callbacks")
 
 
 def test_timer_cancel_reset_10hertz():
@@ -188,8 +185,7 @@ def test_timer_cancel_reset_100hertz():
 
 
 def test_timer_cancel_reset_1000hertz():
-    # if os.name == 'nt' or platform.machine() == 'aarch64':
-    #     raise SkipTest
-    # func_launch(
-    #     func_cancel_reset_timer, ['0.001'], "didn't receive the expected number of callbacks")
-    raise SkipTest
+    if os.name == 'nt' or platform.machine() == 'aarch64':
+        raise SkipTest
+    func_launch(
+        func_cancel_reset_timer, ['0.001'], "didn't receive the expected number of callbacks")
