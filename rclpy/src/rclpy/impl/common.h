98708470075bbbbe819892d24af533f26ed25f5d
Jacob Perron <jacob@openrobotics.org>
2019-01-25 18:13:53 -0800

    Improve error handling and add missing DECREF in rclpy_convert_to_py_qos_policy()

--- rclpy/src/rclpy/impl/common.h
+++ rclpy/src/rclpy/impl/common.h
@@ -32,22 +32,42 @@ static PyObject *
 rclpy_convert_to_py_qos_policy(void * profile)
 {
   PyObject * pyqos_module = PyImport_ImportModule("rclpy.qos");
+  if (!pyqos_module) {
+    return NULL;
+  }
+
   PyObject * pyqos_policy_class = PyObject_GetAttrString(pyqos_module, "QoSProfile");
-  PyObject * pyqos_profile = NULL;
+  if (!pyqos_policy_class) {
+    return PyErr_Format(PyExc_RuntimeError,
+             "Failed to find the class 'QosProfile' in the module 'rclpy.qos'");
+  }
+
+  PyObject * pyqos_profile = PyObject_CallObject(pyqos_policy_class, NULL);
+  Py_DECREF(pyqos_policy_class);
+  if (!pyqos_profile) {
+    return PyErr_Format(PyExc_RuntimeError, "Failed to call object QosProfile");
+  }
+
   rmw_qos_profile_t * qos_profile = (rmw_qos_profile_t *)profile;
-  pyqos_profile = PyObject_CallObject(pyqos_policy_class, NULL);
-  assert(pyqos_profile != NULL);
 
-  PyObject_SetAttrString(pyqos_profile, "depth", PyLong_FromSize_t(qos_profile->depth));
-  PyObject_SetAttrString(pyqos_profile, "history", PyLong_FromUnsignedLong(qos_profile->history));
-  PyObject_SetAttrString(pyqos_profile, "reliability",
-    PyLong_FromUnsignedLong(qos_profile->reliability));
-  PyObject_SetAttrString(pyqos_profile, "durability",
-    PyLong_FromUnsignedLong(qos_profile->durability));
-  PyObject_SetAttrString(pyqos_profile, "avoid_ros_namespace_conventions",
-    PyBool_FromLong(qos_profile->avoid_ros_namespace_conventions));
+  // A success returns 0, and a failure returns -1
+  int set_ret = 0;
+  set_ret += PyObject_SetAttrString(pyqos_profile,
+      "depth", PyLong_FromSize_t(qos_profile->depth));
+  set_ret += PyObject_SetAttrString(pyqos_profile,
+      "history", PyLong_FromUnsignedLong(qos_profile->history));
+  set_ret += PyObject_SetAttrString(pyqos_profile,
+      "reliability", PyLong_FromUnsignedLong(qos_profile->reliability));
+  set_ret += PyObject_SetAttrString(pyqos_profile,
+      "durability", PyLong_FromUnsignedLong(qos_profile->durability));
+  set_ret += PyObject_SetAttrString(pyqos_profile,
+      "avoid_ros_namespace_conventions",
+      PyBool_FromLong(qos_profile->avoid_ros_namespace_conventions));
 
-  assert(pyqos_profile != NULL);
+  if (set_ret < 0) {
+    Py_DECREF(pyqos_profile);
+    return NULL;
+  }
   return pyqos_profile;
 }
 
