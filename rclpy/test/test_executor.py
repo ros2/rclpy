1a3be50798e3c5a37228db5611d48e82e6fefe0e
Shane Loretz <shane.loretz@gmail.com>
2019-04-08 08:29:39 -0700

    Executor exits immediately when shut down (#309)
    * Executor exist immediately when shut down
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Spin doesn't raise ShutdownException
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/test/test_executor.py
+++ rclpy/test/test_executor.py
@@ -59,6 +59,33 @@ class TestExecutor(unittest.TestCase):
         finally:
             executor.shutdown()
 
+    def test_executor_immediate_shutdown(self):
+        self.assertIsNotNone(self.node.handle)
+        executor = SingleThreadedExecutor(context=self.context)
+        try:
+            got_callback = False
+
+            def timer_callback():
+                nonlocal got_callback
+                got_callback = True
+
+            timer_period = 1
+            tmr = self.node.create_timer(timer_period, timer_callback)
+
+            self.assertTrue(executor.add_node(self.node))
+            t = threading.Thread(target=executor.spin, daemon=True)
+            start_time = time.monotonic()
+            t.start()
+            executor.shutdown()
+            t.join()
+            end_time = time.monotonic()
+
+            self.node.destroy_timer(tmr)
+            self.assertLess(end_time - start_time, timer_period / 2)
+            self.assertFalse(got_callback)
+        finally:
+            executor.shutdown()
+
     def test_remove_node(self):
         self.assertIsNotNone(self.node.handle)
         executor = SingleThreadedExecutor(context=self.context)
