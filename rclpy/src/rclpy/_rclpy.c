4ef3d8b1e98094cdf0f50668a60819ec70349efb
Shane Loretz <sloretz@osrfoundation.org>
2019-04-26 10:09:02 -0700

    WAITSETBUG: Get ready entities by rcl type
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/src/rclpy/_rclpy.c
+++ rclpy/src/rclpy/_rclpy.c
@@ -2624,7 +2624,7 @@ rclpy_destroy_wait_set(PyObject * Py_UNUSED(self), PyObject * args)
   const rcl_ ## ENTITY_TYPE ## _t ** struct_ptr = wait_set->ENTITY_TYPE ## s; \
   for (idx = 0; idx < idx_max; idx ++) { \
     if (struct_ptr[idx]) { \
-      PyObject * obj = PyLong_FromUnsignedLongLong((uint64_t) & struct_ptr[idx]->impl); \
+      PyObject * obj = PyLong_FromVoidPtr((void *) struct_ptr[idx]); \
       if (obj) { \
         int rc = PyList_Append(entity_ready_list, obj); \
         Py_DECREF(obj); \
