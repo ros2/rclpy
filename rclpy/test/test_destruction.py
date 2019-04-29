d35fb6d5c54bac7c0993c20f607ef56d8c36a5cc
Shane Loretz <sloretz@osrfoundation.org>
2019-04-29 12:35:31 -0700

    Refactor to not use multiprocessing
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/test/test_destruction.py
+++ rclpy/test/test_destruction.py
@@ -12,194 +12,88 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import multiprocessing
-import sys
-import traceback
-
 import pytest
 import rclpy
 from rclpy.handle import InvalidHandle
 from test_msgs.msg import Primitives
 
 
-def run_catch_report_raise(func, *args, **kwargs):
-    try:
-        return func(*args, **kwargs)
-    except Exception:
-        print('exception in {0}():'.format(func.__name__), file=sys.stderr)
-        traceback.print_exc()
-        raise
-
-
-def func_destroy_node():
-    import rclpy
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_node1', context=context)
-    ret = True
-    try:
-        node.destroy_node()
-    except RuntimeError:
-        ret = False
-    finally:
-        rclpy.shutdown(context=context)
-    return ret
-
-
-def func_destroy_node_twice():
-    import rclpy
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_node2', context=context)
-    assert node.destroy_node() is True
-    assert node.destroy_node() is True
-    return True
-
-
-def func_destroy_corrupted_node():
-    import rclpy
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_node2', context=context)
-    assert node.destroy_node() is True
-    org_handle = node._handle
-    node._handle = 'garbage'
-    ret = False
-    try:
-        node.destroy_node()
-    except ValueError:
-        ret = True
-        node._handle = org_handle
-        node.destroy_node()
-    finally:
-        rclpy.shutdown(context=context)
-    return ret
-
-
-def func_destroy_timers():
-    import rclpy
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-
-    node = rclpy.create_node('test_node3', context=context)
-
-    timer1 = node.create_timer(0.1, None)
-    timer2 = node.create_timer(1, None)
-    timer2  # noqa
-
-    assert 2 == len(node.timers)
-
-    assert node.destroy_timer(timer1) is True
-
-    assert 1 == len(node.timers)
-    try:
-        assert node.destroy_node() is True
-    except SystemError:
-        ret = False
-    else:
-        assert 0 == len(node.timers)
-        assert node.handle is None
-        ret = True
-    finally:
-        rclpy.shutdown(context=context)
-    return ret
-
-
-def func_destroy_entities():
-    import rclpy
-    from test_msgs.msg import Primitives
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-
-    node = rclpy.create_node('test_node4', context=context)
-
-    timer = node.create_timer(0.1, None)
-    timer  # noqa
-    assert 1 == len(node.timers)
-    pub1 = node.create_publisher(Primitives, 'pub1_topic')
-    assert 2 == len(node.publishers)
-    pub2 = node.create_publisher(Primitives, 'pub2_topic')
-    pub2  # noqa
-    assert 3 == len(node.publishers)
-    sub1 = node.create_subscription(
-        Primitives, 'sub1_topic', lambda msg: print('Received %r' % msg))
-    assert 1 == len(node.subscriptions)
-    sub2 = node.create_subscription(
-        Primitives, 'sub2_topic', lambda msg: print('Received %r' % msg))
-    sub2  # noqa
-    assert 2 == len(node.subscriptions)
-
-    assert node.destroy_publisher(pub1) is True
-    assert 2 == len(node.publishers)
-
-    assert node.destroy_subscription(sub1) is True
-    assert 1 == len(node.subscriptions)
-
-    try:
-        assert node.destroy_node() is True
-    except SystemError:
-        ret = False
-    else:
-        assert 0 == len(node.timers)
-        assert 0 == len(node.publishers)
-        assert 0 == len(node.subscriptions)
-        assert node.handle is None
-        ret = True
-    finally:
-        rclpy.shutdown(context=context)
-    return ret
-
-
-def func_corrupt_node_handle():
-    import rclpy
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_node5', context=context)
-    try:
-        node.handle = 'garbage'
-        ret = False
-    except AttributeError:
-        node.destroy_node()
-        ret = True
-    finally:
-        rclpy.shutdown(context=context)
-    return ret
-
-
-def func_launch(function, message):
-    pool = multiprocessing.Pool(1)
-    result = pool.apply(
-        func=run_catch_report_raise,
-        args=(function,)
-    )
-
-    assert result, message
-    pool.close()
-    pool.join()
-
-
 def test_destroy_node():
-    func_launch(func_destroy_node, 'failed to destroy node')
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_node1', context=context)
+        assert node.destroy_node()
+    finally:
+        rclpy.shutdown(context=context)
 
 
 def test_destroy_node_twice():
-    func_launch(func_destroy_node_twice, 'succeeded to destroy same node twice o_O')
-
-
-def test_destroy_corrupted_node():
-    func_launch(func_destroy_corrupted_node, 'destroyed a non existing node o_O')
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_node2', context=context)
+        assert node.destroy_node()
+        assert node.destroy_node()
+    finally:
+        rclpy.shutdown(context=context)
 
 
 def test_destroy_timers():
-    func_launch(func_destroy_timers, 'failed to destroy node entities')
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_node3', context=context)
+        try:
+            timer1 = node.create_timer(0.1, None)
+            timer2 = node.create_timer(1, None)
+            timer2  # noqa
+
+            assert 2 == len(node.timers)
+            assert node.destroy_timer(timer1)
+
+            assert 1 == len(node.timers)
+        finally:
+            node.destroy_node()
+        assert 0 == len(node.timers)
+    finally:
+        rclpy.shutdown(context=context)
 
 
 def test_destroy_entities():
-    func_launch(func_destroy_entities, 'failed to destroy node entities')
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_node4', context=context)
+        try:
+            timer = node.create_timer(0.1, None)
+            timer  # noqa
+            assert 1 == len(node.timers)
+            pub1 = node.create_publisher(Primitives, 'pub1_topic')
+            assert 2 == len(node.publishers)
+            pub2 = node.create_publisher(Primitives, 'pub2_topic')
+            pub2  # noqa
+            assert 3 == len(node.publishers)
+            sub1 = node.create_subscription(
+                Primitives, 'sub1_topic', lambda msg: ...)
+            assert 1 == len(node.subscriptions)
+            sub2 = node.create_subscription(
+                Primitives, 'sub2_topic', lambda msg: ...)
+            sub2  # noqa
+            assert 2 == len(node.subscriptions)
 
+            assert node.destroy_publisher(pub1)
+            assert 2 == len(node.publishers)
 
-def test_corrupt_node_handle():
-    func_launch(func_corrupt_node_handle, 'successfully modified node handle o_O')
+            assert node.destroy_subscription(sub1)
+            assert 1 == len(node.subscriptions)
+        finally:
+            node.destroy_node()
+        assert 0 == len(node.timers)
+        assert 0 == len(node.publishers)
+        assert 0 == len(node.subscriptions)
+    finally:
+        rclpy.shutdown(context=context)
 
 
 def test_destroy_subscription_asap():
--- rclpy/test/test_timer.py
+++ rclpy/test/test_timer.py
@@ -12,188 +12,134 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import multiprocessing
 import os
 import platform
-import sys
 import time
-import traceback
-from unittest.case import SkipTest
 
 import pytest
-
 import rclpy
 from rclpy.executors import SingleThreadedExecutor
 
 
-def run_catch_report_raise(func, *args, **kwargs):
-    try:
-        return func(*args, **kwargs)
-    except Exception:
-        print('exception in {0}():'.format(func.__name__), file=sys.stderr)
-        traceback.print_exc()
-        raise
-
-
-def func_zero_callback(args):
-    period = float(args[0])
-
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_timer_no_callback', context=context)
-    executor = SingleThreadedExecutor(context=context)
-    executor.add_node(node)
-    # The first spin_once() takes slighly longer, just long enough for 1ms timer tests to fail
-    executor.spin_once(timeout_sec=0)
-
-    callbacks = []
-    timer = node.create_timer(period, lambda: callbacks.append(len(callbacks)))
-    executor.spin_once(timeout_sec=period / 2.)
-
-    node.destroy_timer(timer)
-    executor.shutdown()
-    rclpy.shutdown(context=context)
-    assert len(callbacks) == 0, \
-        "shouldn't have received any callback, received %d" % len(callbacks)
-
-    return True
-
-
-def func_number_callbacks(args):
-    period = float(args[0])
-
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_timer_no_callback', context=context)
-    executor = SingleThreadedExecutor(context=context)
-    executor.add_node(node)
-    # The first spin_once() takes slighly longer, just long enough for 1ms timer tests to fail
-    executor.spin_once(timeout_sec=0)
-
-    callbacks = []
-    timer = node.create_timer(period, lambda: callbacks.append(len(callbacks)))
-    begin_time = time.time()
-
-    while rclpy.ok(context=context) and time.time() - begin_time < 4.5 * period:
-        executor.spin_once(timeout_sec=period / 10)
-
-    node.destroy_timer(timer)
-    executor.shutdown()
-    rclpy.shutdown(context=context)
-    assert len(callbacks) == 4, 'should have received 4 callbacks, received %s' % str(callbacks)
-
-    return True
-
-
-def func_cancel_reset_timer(args):
-    period = float(args[0])
-
-    context = rclpy.context.Context()
-    rclpy.init(context=context)
-    node = rclpy.create_node('test_timer_no_callback', context=context)
-    executor = SingleThreadedExecutor(context=context)
-    executor.add_node(node)
-    # The first spin_once() takes slighly longer, just long enough for 1ms timer tests to fail
-    executor.spin_once(timeout_sec=0)
-
-    callbacks = []
-    timer = node.create_timer(period, lambda: callbacks.append(len(callbacks)))
-    begin_time = time.time()
-
-    assert not timer.is_canceled()
-
-    while rclpy.ok(context=context) and time.time() - begin_time < 2.5 * period:
-        executor.spin_once(timeout_sec=period / 10)
-
-    assert len(callbacks) == 2, 'should have received 2 callbacks, received %d' % len(callbacks)
-    assert not timer.is_canceled()
-
-    timer.cancel()
-    assert timer.is_canceled()
-    callbacks = []
-    begin_time = time.time()
-    while rclpy.ok(context=context) and time.time() - begin_time < 2.5 * period:
-        executor.spin_once(timeout_sec=period / 10)
-
-    assert timer.is_canceled()
-    assert [] == callbacks, \
-        "shouldn't have received any callback, received %d" % len(callbacks)
-
-    timer.reset()
-    assert not timer.is_canceled()
-    begin_time = time.time()
-    while rclpy.ok(context=context) and time.time() - begin_time < 2.5 * period:
-        executor.spin_once(timeout_sec=period / 10)
-
-    assert not timer.is_canceled()
-    assert len(callbacks) == 2, 'should have received 2 callbacks, received %d' % len(callbacks)
-    node.destroy_timer(timer)
-    executor.shutdown()
-    rclpy.shutdown(context=context)
-
-    return True
-
-
-def func_launch(function, args, message):
-    pool = multiprocessing.Pool(1)
-    result = pool.apply(
-        func=run_catch_report_raise,
-        args=(function, args)
+TEST_PERIODS = (
+    0.1,
+    0.01,
+    pytest.param(
+        0.001,
+        marks=(
+            pytest.mark.skipif(os.name == 'nt', reason='Flaky on windows'),
+            pytest.mark.skipif(platform.machine() == 'aarch64', reason='Flaky on arm')
+        )
     )
-
-    assert result, message
-    pool.close()
-    pool.join()
+)
 
 
-def test_timer_zero_callbacks10hertz():
-    func_launch(func_zero_callback, ['0.1'], 'received callbacks when not expected')
+@pytest.mark.parametrize('period', TEST_PERIODS)
+def test_zero_callback(period):
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_timer_no_callback', context=context)
+        try:
+            executor = SingleThreadedExecutor(context=context)
+            try:
+                executor.add_node(node)
+                # The first spin_once() takes  long enough for 1ms timer tests to fail
+                executor.spin_once(timeout_sec=0)
+
+                callbacks = []
+                timer = node.create_timer(period, lambda: callbacks.append(len(callbacks)))
+                try:
+                    executor.spin_once(timeout_sec=(period / 2))
+
+                    assert len(callbacks) == 0
+                finally:
+                    node.destroy_timer(timer)
+            finally:
+                executor.shutdown()
+        finally:
+            node.destroy_node()
+    finally:
+        rclpy.shutdown(context=context)
 
 
-def test_timer_zero_callbacks100hertz():
-    func_launch(func_zero_callback, ['0.01'], 'received callbacks when not expected')
+@pytest.mark.parametrize('period', TEST_PERIODS)
+def test_number_callbacks(period):
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_timer_number_callbacks', context=context)
+        try:
+            executor = SingleThreadedExecutor(context=context)
+            try:
+                executor.add_node(node)
+                # The first spin_once() takes  long enough for 1ms timer tests to fail
+                executor.spin_once(timeout_sec=0)
+
+                callbacks = []
+                timer = node.create_timer(period, lambda: callbacks.append(len(callbacks)))
+                try:
+                    begin_time = time.time()
+
+                    while rclpy.ok(context=context) and time.time() - begin_time < 4.5 * period:
+                        executor.spin_once(timeout_sec=period / 10)
+
+                    assert len(callbacks) == 4
+                finally:
+                    node.destroy_timer(timer)
+            finally:
+                executor.shutdown()
+        finally:
+            node.destroy_node()
+    finally:
+        rclpy.shutdown(context=context)
 
 
-# TODO(mikaelarguedas) reenable these once timer have consistent behaviour
-# on every platform at high frequency
-# TODO(sloretz) Reenable on arm when executor performance is good enough
-@pytest.mark.skip(reason='1kHz tests are too prone to flakiness')
-def test_timer_zero_callbacks1000hertz():
-    if os.name == 'nt' or platform.machine() == 'aarch64':
-        raise SkipTest
-    func_launch(func_zero_callback, ['0.001'], 'received callbacks when not expected')
+@pytest.mark.parametrize('period', TEST_PERIODS)
+def test_cancel_reset(period):
+    context = rclpy.context.Context()
+    rclpy.init(context=context)
+    try:
+        node = rclpy.create_node('test_timer_cancel_reset', context=context)
+        try:
+            executor = SingleThreadedExecutor(context=context)
+            try:
+                executor.add_node(node)
+                # The first spin_once() takes  long enough for 1ms timer tests to fail
+                executor.spin_once(timeout_sec=0)
 
+                callbacks = []
+                timer = node.create_timer(period, lambda: callbacks.append(len(callbacks)))
+                try:
+                    # Make sure callbacks can be received
+                    assert not timer.is_canceled()
+                    begin_time = time.time()
+                    while rclpy.ok(context=context) and time.time() - begin_time < 2.5 * period:
+                        executor.spin_once(timeout_sec=period / 10)
+                    assert len(callbacks) == 2
 
-def test_timer_number_callbacks10hertz():
-    func_launch(func_number_callbacks, ['0.1'], "didn't receive the expected number of callbacks")
+                    # Cancel timer and make sure no callbacks are received
+                    assert not timer.is_canceled()
+                    timer.cancel()
+                    assert timer.is_canceled()
+                    callbacks = []
+                    begin_time = time.time()
+                    while rclpy.ok(context=context) and time.time() - begin_time < 2.5 * period:
+                        executor.spin_once(timeout_sec=period / 10)
+                    assert [] == callbacks
 
-
-def test_timer_number_callbacks100hertz():
-    func_launch(
-        func_number_callbacks, ['0.01'], "didn't receive the expected number of callbacks")
-
-
-@pytest.mark.skip(reason='1kHz tests are too prone to flakiness')
-def test_timer_number_callbacks1000hertz():
-    if os.name == 'nt' or platform.machine() == 'aarch64':
-        raise SkipTest
-    func_launch(
-        func_number_callbacks, ['0.001'], "didn't receive the expected number of callbacks")
-
-
-def test_timer_cancel_reset_10hertz():
-    func_launch(
-        func_cancel_reset_timer, ['0.1'], "didn't receive the expected number of callbacks")
-
-
-def test_timer_cancel_reset_100hertz():
-    func_launch(
-        func_cancel_reset_timer, ['0.01'], "didn't receive the expected number of callbacks")
-
-
-@pytest.mark.skip(reason='1kHz tests are too prone to flakiness')
-def test_timer_cancel_reset_1000hertz():
-    if os.name == 'nt' or platform.machine() == 'aarch64':
-        raise SkipTest
-    func_launch(
-        func_cancel_reset_timer, ['0.001'], "didn't receive the expected number of callbacks")
+                    # Reenable timer and make sure callbacks are received again
+                    timer.reset()
+                    assert not timer.is_canceled()
+                    begin_time = time.time()
+                    while rclpy.ok(context=context) and time.time() - begin_time < 2.5 * period:
+                        executor.spin_once(timeout_sec=period / 10)
+                    assert len(callbacks) == 2
+                finally:
+                    node.destroy_timer(timer)
+            finally:
+                executor.shutdown()
+        finally:
+            node.destroy_node()
+    finally:
+        rclpy.shutdown(context=context)
