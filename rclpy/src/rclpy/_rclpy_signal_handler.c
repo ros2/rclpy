f267349f3d0d74c7909e49b0526200cc8164d2d3
Michel Hidalgo <michel@ekumenlabs.com>
2019-05-06 15:45:16 -0300

    Improve signal handling (#338)
    * Improve signal handling
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Address peer review comments.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Fix initializer warnings for clang on MacOS.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Move away from brace initializers
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Please uncrustify
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- rclpy/src/rclpy/_rclpy_signal_handler.c
+++ rclpy/src/rclpy/_rclpy_signal_handler.c
@@ -21,14 +21,147 @@
 
 #include <signal.h>
 
-#ifdef _WIN32
-  #define SIGNAL_HANDLER_T _crt_signal_t
+#if __APPLE__ || _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _POSIX_SOURCE
+
+#define SIGNAL_HANDLER_T struct sigaction
+
+static SIGNAL_HANDLER_T
+make_null_signal_handler()
+{
+  SIGNAL_HANDLER_T handler;
+  memset(&handler, 0, sizeof(handler));
+  return handler;
+}
+
+#define NULL_SIGNAL_HANDLER make_null_signal_handler()
+
+#define DEFINE_SIGNAL_HANDLER(name) \
+  static void _ ## name(int signum, siginfo_t * info, void * context); \
+  static SIGNAL_HANDLER_T get_ ## name() { \
+    SIGNAL_HANDLER_T handler; \
+    memset(&handler, 0, sizeof(handler)); \
+    sigemptyset(&handler.sa_mask); \
+    handler.sa_sigaction = _ ## name; \
+    handler.sa_flags = SA_SIGINFO; \
+    return handler; \
+  } \
+  static bool is_ ## name(SIGNAL_HANDLER_T handler) { \
+    return _ ## name == handler.sa_sigaction; \
+  } \
+  static void _ ## name(int signum, siginfo_t * info, void * context)
+
+#define SIGNAL_HANDLER_ARGS signum, info, context
+
+static void
+call_signal_handler(
+  SIGNAL_HANDLER_T handler, int signum,
+  siginfo_t * siginfo, void * context)
+{
+  if (handler.sa_flags & SA_SIGINFO) {
+    if (handler.sa_sigaction != NULL) {
+      handler.sa_sigaction(signum, siginfo, context);
+    }
+  } else {
+    if (handler.sa_handler != NULL &&    // Is set
+      handler.sa_handler != SIG_DFL &&      // Is not default
+      handler.sa_handler != SIG_IGN)      // Is not ignored
+    {
+      handler.sa_handler(signum);
+    }
+  }
+}
+
+static SIGNAL_HANDLER_T
+install_signal_handler(int signum, SIGNAL_HANDLER_T handler)
+{
+  SIGNAL_HANDLER_T old_handler;
+  sigaction(signum, &handler, &old_handler);
+  return old_handler;
+}
+
+#define is_null_signal_handler(handler) \
+  (handler.sa_flags & SA_SIGINFO ? \
+  NULL == handler.sa_sigaction : \
+  NULL == handler.sa_handler)
+
 #else
-  #define SIGNAL_HANDLER_T sig_t
+
+#ifdef _WIN32
+#define SIGNAL_HANDLER_T _crt_signal_t
+#else
+#define SIGNAL_HANDLER_T sig_t
 #endif  // _WIN32
 
-/// Global reference to original signal handler for chaining purposes
-SIGNAL_HANDLER_T g_original_signal_handler = NULL;
+#define NULL_SIGNAL_HANDLER NULL
+
+#define DEFINE_SIGNAL_HANDLER(name) \
+  static void _ ## name(int signum); \
+  static SIGNAL_HANDLER_T get_ ## name() {return _ ## name;} \
+  static bool is_ ## name(SIGNAL_HANDLER_T handler) { \
+    return _ ## name == handler; \
+  } \
+  static void _ ## name(int signum)
+
+#define SIGNAL_HANDLER_ARGS signum
+
+#define call_signal_handler(handler, ...) handler(__VA_ARGS__)
+
+#define install_signal_handler(signum, handler) signal(signum, handler)
+
+#define is_null_signal_handler(signal_handler) (NULL_SIGNAL_HANDLER == signal_handler)
+
+#endif
+
+// Forward declarations
+static bool trigger_guard_conditions();
+static void unregister_sigint_signal_handler();
+
+/// Original signal handler for chaining purposes
+SIGNAL_HANDLER_T g_original_sigint_handler;
+
+/// Signal handler function
+DEFINE_SIGNAL_HANDLER(rclpy_sigint_handler)
+{
+  if (!is_null_signal_handler(g_original_sigint_handler)) {
+    call_signal_handler(g_original_sigint_handler, SIGNAL_HANDLER_ARGS);
+  }
+
+  if (!trigger_guard_conditions()) {
+    // There may have been another signal handler chaining to this
+    // one when we last tried to unregister ourselves.
+
+    // Try to unregister again.
+    unregister_sigint_signal_handler();
+  }
+}
+
+/// Unregister our SIGINT handler and restore the original one
+static void
+unregister_sigint_signal_handler()
+{
+  const SIGNAL_HANDLER_T current_sigint_handler =
+    install_signal_handler(SIGINT, g_original_sigint_handler);
+  if (!is_rclpy_sigint_handler(current_sigint_handler)) {
+    // Oops, someone else must have registered a signal handler
+    // that chains to us put it back so it continues to work
+    install_signal_handler(SIGINT, current_sigint_handler);
+    return;
+  }
+  // Got ourself out of the chain
+  g_original_sigint_handler = NULL_SIGNAL_HANDLER;
+}
+
+/// Register our SIGINT handler and store the current one
+static void
+register_sigint_signal_handler()
+{
+  if (!is_null_signal_handler(g_original_sigint_handler)) {
+    // Handler already registered
+    return;
+  }
+  g_original_sigint_handler =
+    install_signal_handler(SIGINT, get_rclpy_sigint_handler());
+}
 
 typedef _Atomic (rcl_guard_condition_t **) atomic_rcl_guard_condition_ptrptr_t;
 
@@ -53,60 +186,19 @@ check_signal_safety()
   return 0;
 }
 
-// Forward declaration
-static void catch_function(int signo);
-
-/// Restore the original signal handler when ours was registered
-static void
-restore_original_signal_handler()
-{
-  const SIGNAL_HANDLER_T current_handler = signal(SIGINT, g_original_signal_handler);
-  if (current_handler != catch_function) {
-    // Oops, someone else must have registered a signal handler that chains to us
-    // put it back so it continues to work
-    signal(SIGINT, current_handler);
-    return;
-  }
-  // Got ourself out of the chain
-  g_original_signal_handler = NULL;
-}
-
-/// Register our signal handler and store the current
-static void
-register_signal_handler()
-{
-  if (NULL != g_original_signal_handler) {
-    // We must already be registered
-    return;
-  }
-  g_original_signal_handler = signal(SIGINT, catch_function);
-}
-
-/// Call the original signal handler if there was one
-static void
-call_original_signal_handler(int signo)
-{
-  if (NULL != g_original_signal_handler) {
-    g_original_signal_handler(signo);
-  }
-}
-
-/// Catch signals
+/// Trigger all registered guard conditions
 /**
  * This triggers guard conditions when a signal is received.
  * These wake executors currently blocked in `rcl_wait`.
+ * Returns True if at least one guard condition was triggered.
  */
-static void catch_function(int signo)
+static bool
+trigger_guard_conditions()
 {
   rcl_guard_condition_t ** guard_conditions;
   rcutils_atomic_load(&g_guard_conditions, guard_conditions);
   if (NULL == guard_conditions || NULL == guard_conditions[0]) {
-    call_original_signal_handler(signo);
-    // There may have been another signal handler chaining to this one when we last tried to
-    // restore the old signal handler.
-    // Try to restore the old handler again.
-    restore_original_signal_handler();
-    return;
+    return false;
   }
 
   rcl_guard_condition_t ** pgc = guard_conditions;
@@ -119,9 +211,7 @@ static void catch_function(int signo)
     }
     ++pgc;
   }
-
-  // Chain signal handlers
-  call_original_signal_handler(signo);
+  return true;
 }
 
 /// Register a guard condition to be triggered when SIGINT is received.
@@ -188,7 +278,7 @@ rclpy_register_sigint_guard_condition(PyObject * Py_UNUSED(self), PyObject * arg
   }
 
   // make sure our signal handler is registered
-  register_signal_handler();
+  register_sigint_signal_handler();
 
   Py_RETURN_NONE;
 }
@@ -247,7 +337,7 @@ rclpy_unregister_sigint_guard_condition(PyObject * Py_UNUSED(self), PyObject * a
     rcl_guard_condition_t ** old_gcs;
     rcutils_atomic_exchange(&g_guard_conditions, old_gcs, NULL);
     allocator.deallocate(old_gcs, allocator.state);
-    restore_original_signal_handler();
+    unregister_sigint_signal_handler();
   } else {
     // Create space for one less guard condition
     // current list size: count_gcs + 1 (sentinel)
@@ -312,5 +402,6 @@ static struct PyModuleDef _rclpy_signal_handler_module = {
 PyMODINIT_FUNC PyInit__rclpy_signal_handler(void)
 {
   atomic_init(&g_guard_conditions, NULL);
+  g_original_sigint_handler = NULL_SIGNAL_HANDLER;
   return PyModule_Create(&_rclpy_signal_handler_module);
 }
