0be24021af4269ce7c83fe9db550e326a8428a9b
Jacob Perron <jacob@openrobotics.org>
2019-06-07 11:45:59 -0700

    Fix API documentation related to ROS graph methods (#366)
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- rclpy/rclpy/action/graph.py
+++ rclpy/rclpy/action/graph.py
@@ -23,14 +23,16 @@ def get_action_client_names_and_types_by_node(
     node: Node,
     remote_node_name: str,
     remote_node_namespace: str
-) -> List[Tuple[str, str]]:
+) -> List[Tuple[str, List[str]]]:
     """
     Get a list of action names and types for action clients associated with a node.
 
     :param node: The node used for discovery.
     :param remote_node_name: The name of a remote node to get action clients for.
     :param node_namespace: Namespace of the remote node.
-    :return: List of tuples containing two strings: the action name and action type.
+    :return: List of tuples.
+      The first element of each tuple is the action name and the second element is a list of
+      action types.
     """
     with node.handle as node_capsule:
         return _rclpy_action.rclpy_action_get_client_names_and_types_by_node(
@@ -41,27 +43,30 @@ def get_action_server_names_and_types_by_node(
     node: Node,
     remote_node_name: str,
     remote_node_namespace: str
-) -> List[Tuple[str, str]]:
+) -> List[Tuple[str, List[str]]]:
     """
     Get a list of action names and types for action servers associated with a node.
 
     :param node: The node used for discovery.
     :param remote_node_name: The name of a remote node to get action servers for.
     :param node_namespace: Namespace of the remote node.
-    :return: List of tuples containing two strings: the action name and action type.
+    :return: List of tuples.
+      The first element of each tuple is the action name and the second element is a list of
+      action types.
     """
     with node.handle as node_capsule:
         return _rclpy_action.rclpy_action_get_server_names_and_types_by_node(
             node_capsule, remote_node_name, remote_node_namespace)
 
 
-def get_action_names_and_types(node: Node) -> List[Tuple[str, str]]:
+def get_action_names_and_types(node: Node) -> List[Tuple[str, List[str]]]:
     """
     Get a list of action names and types.
 
     :param node: The node used for discovery.
-    :return: List of action names and types in the ROS graph as tuples containing two
-       strings: the action name and action type.
+    :return: List of action names and types in the ROS graph as tuples.
+      The first element of each tuple is the action name and the second element is a list of
+      action types.
     """
     with node.handle as node_capsule:
         return _rclpy_action.rclpy_action_get_names_and_types(node_capsule)
