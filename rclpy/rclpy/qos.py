9ef6b2e9f89dc7217de48f2882f27a70a93aed6c
Abhinav <singhabhinav9051571833@gmail.com>
2019-06-25 13:59:50 +0530

    G
    Signed-off-by: Abhinav <singhabhinav9051571833@gmail.com>

--- rclpy/rclpy/qos.py
+++ rclpy/rclpy/qos.py
@@ -329,7 +329,6 @@ class QoSLivelinessPolicy(QoSPolicyEnum):
 # Alias with the old name, for retrocompatibility is clearer.
 QoSLivelinessPolicy = LivelinessPolicy
 
-
 class DeprecatedQoSProfile(QoSProfile):
 
     def __init__(self, qos_profile: QoSProfile, profile_name: str):
