5de23a565ea2ba884b012e57deaa9c0b0ccbce9c
Siddharth Kucheria <kucheria@usc.edu>
2019-07-08 15:54:45 -0700

    Update test_node.py

--- rclpy/test/test_node.py
+++ rclpy/test/test_node.py
@@ -176,10 +176,12 @@ class TestNodeAllowUndeclaredParameters(unittest.TestCase):
         # test that it doesn't raise
         self.node.get_service_names_and_types()
 
+    def test_service_names_and_types_by_node(self):
+        # test that it doesnt raise
+        self.node.get_service_names_and_types_by_node(TEST_NODE, TEST_NAMESPACE)
+        
     def test_client_names_and_types_by_node(self):
         # test that it doesnt raise
-        # node1 = rclpy.create_node(
-        #         'my_node', namespace='/my_ns', use_global_arguments=True, context=context)
         self.node.get_client_names_and_types_by_node(TEST_NODE, TEST_NAMESPACE)
 
     def test_topic_names_and_types(self):
