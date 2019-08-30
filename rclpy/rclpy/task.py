60faa2a677bd35a034044d01acaef3dad843ddaa
Shane Loretz <sloretz@osrfoundation.org>
2019-08-30 10:46:45 -0700

    Future result() and __await__ raise exceptions
    Matches the behavior of `result` and `__await__` on asyncio Future's,
    and makes them a little more convenient to use.
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/rclpy/task.py
+++ rclpy/rclpy/task.py
@@ -54,7 +54,7 @@ class Future:
         # Yield if the task is not finished
         while not self._done:
             yield
-        return self._result
+        return self.result()
 
     def cancel(self):
         """Request cancellation of the running task if it is not done already."""
@@ -85,8 +85,12 @@ class Future:
         """
         Get the result of a done task.
 
+        This raises an exception if one was set during the task.
+
         :return: The result set by the task
         """
+        if self._exception:
+            raise self.exception()
         return self._result
 
     def exception(self):
