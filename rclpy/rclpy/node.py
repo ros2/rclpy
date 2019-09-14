d5ae2dc27937f01043a06141b901fa913986daeb
AbhinavSingh <singhabhinav9051571833@gmail.com>
2019-09-14 16:03:28 +0530

    Signed-off-by: AbhinavSingh <singhabhinav9051571833@gmail.com> @ivanpauno Is my add_on_set_parameter_callback() correct?

--- rclpy/rclpy/node.py
+++ rclpy/rclpy/node.py
@@ -141,7 +141,7 @@ class Node:
         self.__guards: List[GuardCondition] = []
         self.__waitables: List[Waitable] = []
         self._default_callback_group = MutuallyExclusiveCallbackGroup()
-        self._parameters_callback = None
+        self._parameters_callback:List[List[Parameter],SetParametersResult]=[]
         self._allow_undeclared_parameters = allow_undeclared_parameters
         self._parameter_overrides = {}
         self._descriptors = {}
@@ -759,9 +759,17 @@ class Node:
     
     def add_on_set_paramters_callback(self,Callable:Callable([List[Parameter],SetParametersResult])):
         """add the callback to list"""
+        prevCallBack=self._parameters_callback
+        prevCallBack.insert(0,Callable)
+        self._parameters_callback=prevCallBack
+
     
     def remove_on_set_parameters_callback(self,Callable:Callable[List[Parameter],SetParametersResult]):
         """remove callback from list"""
+        # prevCallBack=self._parameters_callback
+        
+
+        # self._parameters_callback=prevCallBack
 
     def _apply_descriptors(
         self,
@@ -1021,7 +1029,7 @@ class Node:
         """
         Register a set parameters callback.
 
-        Calling this function with override any previously registered callback.
+        Calling this function will override any previously registered callback.
 
         :param callback: The function that is called whenever parameters are set for the node.
         """
