585b41a907b5c5387f89363af45e5aa03e69a694
Jaison Titus <jaisontj92@gmail.com>
2019-11-15 14:12:21 -0800

    Fixed code formatting issues.
    Signed-off-by: Jaison Titus <jaisontj92@gmail.com>

--- rclpy/src/rclpy/_rclpy.c
+++ rclpy/src/rclpy/_rclpy.c
@@ -921,7 +921,7 @@ _get_info_by_topic(
         type, rcl_get_error_string().str);
     } else if (ret == RCL_RET_UNSUPPORTED) {
       PyErr_Format(PyExc_NotImplementedError, "Failed to get information by topic for %s: "
-                                       "function not supported by RMW_IMPLEMENTATION", type);
+        "function not supported by RMW_IMPLEMENTATION", type);
     } else {
       PyErr_Format(PyExc_RuntimeError, "Failed to get information by topic for %s: %s",
         type, rcl_get_error_string().str);
@@ -929,7 +929,8 @@ _get_info_by_topic(
     rcl_reset_error();
     fini_ret = rmw_topic_info_array_fini(&allocator, &info_array);
     if (fini_ret != RMW_RET_OK) {
-      PyErr_Format(PyExc_RuntimeError, "rmw_topic_info_array_fini failed: %s", rmw_get_error_string().str);
+      PyErr_Format(PyExc_RuntimeError, "rmw_topic_info_array_fini failed: %s",
+        rmw_get_error_string().str);
       rmw_reset_error();
     }
     return NULL;
