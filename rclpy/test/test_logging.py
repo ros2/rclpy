801996cdd88e9959d7e9977950ec4723b44e2438
Brian Marchi <brian.marchi65@gmail.com>
2019-11-26 14:46:47 -0300

    Provide logging severity for string (#458)
    * Provide logging severity for string
    
    Signed-off-by: Brian Ezequiel Marchi <brian.marchi65@gmail.com>

--- rclpy/test/test_logging.py
+++ rclpy/test/test_logging.py
@@ -359,6 +359,15 @@ class TestLogging(unittest.TestCase):
             rclpy.logging._root_logger.get_effective_level(),
             my_logger.get_effective_level())
 
+    def test_logging_severity_from_string(self):
+        for severity in rclpy.logging.LoggingSeverity:
+            self.assertEqual(
+                rclpy.logging.get_logging_severity_from_string(severity.name), severity)
+
+    def test_nonexistent_logging_severity_from_string(self):
+        with self.assertRaises(RuntimeError):
+            rclpy.logging.get_logging_severity_from_string('non_existent_severity')
+
 
 if __name__ == '__main__':
     unittest.main()
