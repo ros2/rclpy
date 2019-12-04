b1732da4244be8e4356549eb637bacd935f2bf50
Barry Xu <Barry.Xu@sony.com>
2019-12-04 11:18:12 +0800

    use pytest parametrize to instead of python3-parameterized
    Signed-off-by: Barry Xu <Barry.Xu@sony.com>

--- rclpy/test/test_logging_rosout.py
+++ rclpy/test/test_logging_rosout.py
@@ -12,73 +12,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
+import pytest
 
-from parameterized import parameterized
 from rcl_interfaces.msg import Log
 import rclpy
 from rclpy.executors import SingleThreadedExecutor
 
+TEST_PARAMETERS = [
+    ('enable_global_rosout_enable_node_rosout', True, True, True),
+    ('enable_global_rosout_disable_node_rosout', True, False, False),
+    ('disable_global_rosout_enable_node_rosout', False, True, False),
+    ('disable_global_rosout_disable_node_rosout', False, False, False),
+]
 
-class TestLoggingRosout(unittest.TestCase):
-
-    @parameterized.expand([
-        ['enable_global_rosout_enable_node_rosout', True, True, True],
-        ['enable_global_rosout_disable_node_rosout', True, False, False],
-        ['disable_global_rosout_enable_node_rosout', False, True, False],
-        ['disable_global_rosout_disable_node_rosout', False, False, False],
-    ])
-    def test_enable_rosout(
-        self,
-        name,
-        enable_global_rosout_logs,
-        enable_node_rosout,
-        expected_data
-    ):
-        if enable_global_rosout_logs:
-            args = ['--ros-args', '--enable-rosout-logs']
-        else:
-            args = ['--ros-args', '--disable-rosout-logs']
-
-        context = rclpy.context.Context()
-        rclpy.init(context=context, args=args)
-        executor = SingleThreadedExecutor(context=context)
-
-        # create node
-        node = rclpy.create_node(
-            node_name='my_node_'+name,
-            namespace='/my_ns',
-            enable_rosout=enable_node_rosout,
-            context=context
-        )
-        executor.add_node(node)
-
-        # create subscriber of 'rosout' topic
-        self.raw_subscription_msg = None  # None=No result yet
-        node.create_subscription(
-            Log,
-            'rosout',
-            self.raw_subscription_callback,
-            1,
-            raw=True
-        )
-
-        node.get_logger().info('SOMETHING')
-        executor.spin_once(timeout_sec=1)
-        if expected_data:
-            self.assertIsNotNone(self.raw_subscription_msg, 'raw subscribe timed out')
-            self.assertIs(
-                type(self.raw_subscription_msg), bytes, 'raw subscribe did not return bytes'
-            )
-            self.assertNotEqual(len(self.raw_subscription_msg), 0, 'raw subscribe invalid length')
-        else:
-            self.assertIsNone(self.raw_subscription_msg)
-        node.destroy_node()
-        rclpy.shutdown(context=context)
-
-    def raw_subscription_callback(self, msg):
-        self.raw_subscription_msg = msg
+raw_subscription_msg = None  # None=No result yet
 
 
-if __name__ == '__main__':
-    unittest.main()
+def raw_subscription_callback(msg):
+    global raw_subscription_msg
+    raw_subscription_msg = msg
+
+
+@pytest.mark.parametrize(
+    'name,enable_global_rosout_logs,enable_node_rosout,expected_data',
+    TEST_PARAMETERS)
+def test_enable_rosout(
+    name,
+    enable_global_rosout_logs,
+    enable_node_rosout,
+    expected_data
+):
+    if enable_global_rosout_logs:
+        args = ['--ros-args', '--enable-rosout-logs']
+    else:
+        args = ['--ros-args', '--disable-rosout-logs']
+
+    context = rclpy.context.Context()
+    rclpy.init(context=context, args=args)
+    executor = SingleThreadedExecutor(context=context)
+
+    # create node
+    node = rclpy.create_node(
+        node_name='my_node_'+name,
+        namespace='/my_ns',
+        enable_rosout=enable_node_rosout,
+        context=context
+    )
+    executor.add_node(node)
+
+    global raw_subscription_msg
+    raw_subscription_msg = None
+    # create subscriber of 'rosout' topic
+    node.create_subscription(
+        Log,
+        'rosout',
+        raw_subscription_callback,
+        1,
+        raw=True
+    )
+
+    node.get_logger().info('SOMETHING')
+    executor.spin_once(timeout_sec=1)
+
+    if expected_data:
+        assert (raw_subscription_msg is not None)
+        assert (type(raw_subscription_msg) == bytes)
+        assert (len(raw_subscription_msg) != 0)
+    else:
+        assert (raw_subscription_msg is None)
+
+    node.destroy_node()
+    rclpy.shutdown(context=context)
