d21672764af4ed4521ff441ec2a0aa4b19e046ac
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-04-10 16:10:05 -0400

    Convert to f-strings per feedback.
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>

--- rclpy/rclpy/exceptions.py
+++ rclpy/rclpy/exceptions.py
@@ -71,8 +71,7 @@ class ParameterException(Exception):
     """Base exception for parameter-related errors."""
 
     def __init__(self, error_msg, parameters, *args):
-        Exception.__init__(self, '{error_msg}: {parameters}'.format(
-            error_msg=error_msg, parameters=parameters), *args)
+        Exception.__init__(self, f'{error_msg}: {parameters}')
 
 
 class ParameterNotDeclaredException(ParameterException):
