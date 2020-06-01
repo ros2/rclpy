69aad495b03bf14a8abc86ed176a05477b29c728
William Woodall <william@osrfoundation.org>
2020-06-01 10:38:50 -0700

    protect access to global logging calls with a mutex (#562)
    * protect access to global logging calls with a mutex
    
    Signed-off-by: William Woodall <william@osrfoundation.org>
    
    * address review comments and fix style
    
    Signed-off-by: William Woodall <william@osrfoundation.org>

---
+++ rclpy/src/rclpy/detail/execute_with_logging_mutex.cpp
@@ -0,0 +1,72 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "./execute_with_logging_mutex.h"  // NOLINT(build/include)
+
+// Must be before Python.h; makes #-formats use ssize_t instead of int
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include "rcutils/logging.h"
+#include "rcutils/visibility_control_macros.h"
+
+#include "./logging_mutex.hpp"
+
+extern "C"
+{
+PyObject *
+rclpy_detail_execute_with_logging_mutex(
+  rclpy_detail_execute_with_logging_mutex_sig function,
+  PyObject * self,
+  PyObject * args)
+{
+  try {
+    std::lock_guard<std::recursive_mutex> guard(rclpy::detail::get_global_logging_mutex());
+    return function(self, args);
+  } catch (std::exception & ex) {
+    PyErr_Format(
+      PyExc_RuntimeError,
+      "Failed to acquire logging mutex: %s", ex.what());
+    return NULL;
+  } catch (...) {
+    PyErr_Format(
+      PyExc_RuntimeError,
+      "Failed to acquire logging mutex");
+    return NULL;
+  }
+}
+
+void
+rclpy_detail_execute_with_logging_mutex2(
+  rclpy_detail_execute_with_logging_mutex_sig2 function,
+  void * arg)
+{
+  try {
+    std::lock_guard<std::recursive_mutex> guard(rclpy::detail::get_global_logging_mutex());
+    function(arg);
+  } catch (std::exception & ex) {
+    // Warning should use line number of the current stack frame
+    int stack_level = 1;
+    PyErr_WarnFormat(
+      PyExc_RuntimeWarning, stack_level,
+      "Failed to acquire logging mutex: %s", ex.what());
+  } catch (...) {
+    // Warning should use line number of the current stack frame
+    int stack_level = 1;
+    PyErr_WarnFormat(
+      PyExc_RuntimeWarning, stack_level,
+      "Failed to acquire logging mutex");
+  }
+}
+}  // extern "C"
---
+++ rclpy/src/rclpy/detail/execute_with_logging_mutex.h
@@ -0,0 +1,56 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RCLPY__DETAIL__EXECUTE_WITH_LOGGING_MUTEX_H_
+#define RCLPY__DETAIL__EXECUTE_WITH_LOGGING_MUTEX_H_
+
+// Must be before Python.h; makes #-formats use ssize_t instead of int
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include "rcutils/logging.h"
+#include "rcutils/visibility_control_macros.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef PyObject * (* rclpy_detail_execute_with_logging_mutex_sig)(PyObject *, PyObject *);
+
+/// Execute the given function pointer after acquiring the logging mutex.
+RCUTILS_LOCAL
+PyObject *
+rclpy_detail_execute_with_logging_mutex(
+  rclpy_detail_execute_with_logging_mutex_sig function,
+  PyObject * self,
+  PyObject * args);
+
+typedef void (* rclpy_detail_execute_with_logging_mutex_sig2)(void *);
+
+/// Execute the given function pointer after acquiring the logging mutex.
+/**
+ * This version has a slightly different signature which is used when destroying a node.
+ */
+RCUTILS_LOCAL
+void
+rclpy_detail_execute_with_logging_mutex2(
+  rclpy_detail_execute_with_logging_mutex_sig2 function,
+  void * arg);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // RCLPY__DETAIL__EXECUTE_WITH_LOGGING_MUTEX_H_
