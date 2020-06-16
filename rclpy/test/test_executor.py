2cc892b007df07d2ff36af5a10113851d06209d0
Audrow <audrow.nash@gmail.com>
2020-06-16 10:18:37 -0700

    Use assertRaises context to avoid lambda functions
    Signed-off-by: Audrow <audrow.nash@gmail.com>

--- rclpy/test/test_executor.py
+++ rclpy/test/test_executor.py
@@ -93,10 +93,8 @@ class TestExecutor(unittest.TestCase):
         for cls in [SingleThreadedExecutor, MultiThreadedExecutor]:
             executor = cls(context=self.context)
             executor.shutdown()
-            self.assertRaises(
-                ShutdownException,
-                executor.wait_for_ready_callbacks
-            )
+            with self.assertRaises(ShutdownException):
+                executor.wait_for_ready_callbacks()
 
     def test_shutdown_exception_from_callback_generator(self):
         self.assertIsNotNone(self.node.handle)
@@ -104,10 +102,8 @@ class TestExecutor(unittest.TestCase):
             executor = cls(context=self.context)
             cb_generator = executor._wait_for_ready_callbacks()
             executor.shutdown()
-            self.assertRaises(
-                ShutdownException,
-                lambda: next(cb_generator)
-            )
+            with self.assertRaises(ShutdownException):
+                next(cb_generator)
 
     def test_remove_node(self):
         self.assertIsNotNone(self.node.handle)
