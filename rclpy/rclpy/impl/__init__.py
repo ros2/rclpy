81c024f3a7890bb76ef2b9cb061ef0991fb7d08d
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-07-08 20:26:24 -0700

    wrap lines to pass linter
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- rclpy/rclpy/impl/__init__.py
+++ rclpy/rclpy/impl/__init__.py
@@ -23,7 +23,7 @@ def _import(name):
         if e.path is not None and os.path.isfile(e.path):
             e.msg += \
                 "\nThe C extension '%s' failed to be imported while being present on the system." \
-                " Please refer to '%s' for possible solutions" % \
-                (e.path, 'https://index.ros.org/doc/ros2/Troubleshooting/Installation-Troubleshooting/'
-                         '#import-failing-even-with-library-present-on-the-system')
+                " Please refer to 'https://index.ros.org/doc/ros2/Troubleshooting/Installation-" \
+                "Troubleshooting/#import-failing-even-with-library-present-on-the-system' for " \
+                'possible solutions' % e.path
         raise
