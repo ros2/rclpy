80781c97e1365aaa221b23e2d5fba75f7341a3f6
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-10-28 13:54:25 -0300

    Add test cases
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rclpy/test/test_qos_overriding_options.py
+++ rclpy/test/test_qos_overriding_options.py
@@ -12,11 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
+
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 from rclpy.publisher import Publisher
 from rclpy.qos import QoSPolicyKind, QoSProfile
-from rclpy.qos_overriding_options import _declare_qos_parameteres, QoSOverridingOptions
+from rclpy.qos_overriding_options import _declare_qos_parameteres
+from rclpy.qos_overriding_options import InvalidQosOverridesError
+from rclpy.qos_overriding_options import QoSOverridingOptions
 
 
 def test_declare_qos_parameters():
@@ -24,6 +29,97 @@ def test_declare_qos_parameters():
     node = Node("my_node")
     _declare_qos_parameteres(
         Publisher, node, '/my_topic', QoSProfile(depth=10),
-        QoSOverridingOptions(policy_kinds=(QoSPolicyKind.RELIABILITY, QoSPolicyKind.HISTORY))
+        QoSOverridingOptions.with_default_policies()
     )
+    qos_overrides = node.get_parameters_by_prefix('qos_overrides')
+    assert len(qos_overrides) == 3
+    expected_params = (
+        ('/my_topic.publisher.depth', 10),
+        ('/my_topic.publisher.history', 'keep_last'),
+        ('/my_topic.publisher.reliability', 'reliable'),
+    )
+    for actual, expected in zip (
+        sorted(qos_overrides.items(), key=lambda x: x[0]), expected_params
+    ):
+        assert actual[0] == expected[0]  # same param name
+        assert actual[1].value == expected[1]  # same param value
+
+    rclpy.shutdown()
+
+
+def test_declare_qos_parameters_with_overrides():
+    rclpy.init()
+    node = Node("my_node", parameter_overrides=[
+        Parameter('qos_overrides./my_topic.publisher.depth', value=100),
+        Parameter('qos_overrides./my_topic.publisher.reliability', value='best_effort'),
+    ])
+    _declare_qos_parameteres(
+        Publisher, node, '/my_topic', QoSProfile(depth=10),
+        QoSOverridingOptions.with_default_policies()
+    )
+    qos_overrides = node.get_parameters_by_prefix('qos_overrides')
+    assert len(qos_overrides) == 3
+    expected_params = (
+        ('/my_topic.publisher.depth', 100),
+        ('/my_topic.publisher.history', 'keep_last'),
+        ('/my_topic.publisher.reliability', 'best_effort'),
+    )
+    for actual, expected in zip (
+        sorted(qos_overrides.items(), key=lambda x: x[0]), expected_params
+    ):
+        assert actual[0] == expected[0]  # same param name
+        assert actual[1].value == expected[1]  # same param value
+
+    rclpy.shutdown()
+
+
+def test_declare_qos_parameters_with_happy_callback():
+    rclpy.init()
+    node = Node("my_node")
+    _declare_qos_parameteres(
+        Publisher, node, '/my_topic', QoSProfile(depth=10),
+        QoSOverridingOptions.with_default_policies(callback=lambda x: True)
+    )
+    qos_overrides = node.get_parameters_by_prefix('qos_overrides')
+    assert len(qos_overrides) == 3
+    expected_params = (
+        ('/my_topic.publisher.depth', 10),
+        ('/my_topic.publisher.history', 'keep_last'),
+        ('/my_topic.publisher.reliability', 'reliable'),
+    )
+    rclpy.shutdown()
+
+
+def test_declare_qos_parameters_with_unhappy_callback():
+    rclpy.init()
+    node = Node("my_node")
+
+    with pytest.raises(InvalidQosOverridesError):
+        _declare_qos_parameteres(
+            Publisher, node, '/my_topic', QoSProfile(depth=10),
+            QoSOverridingOptions.with_default_policies(callback=lambda x: False)
+        )
+    rclpy.shutdown()
+
+
+def test_declare_qos_parameters_with_id():
+    rclpy.init()
+    node = Node("my_node")
+    _declare_qos_parameteres(
+        Publisher, node, '/my_topic', QoSProfile(depth=10),
+        QoSOverridingOptions.with_default_policies(id='i_have_an_id')
+    )
+    qos_overrides = node.get_parameters_by_prefix('qos_overrides')
+    assert len(qos_overrides) == 3
+    expected_params = (
+        ('/my_topic.publisher_i_have_an_id.depth', 10),
+        ('/my_topic.publisher_i_have_an_id.history', 'keep_last'),
+        ('/my_topic.publisher_i_have_an_id.reliability', 'reliable'),
+    )
+    for actual, expected in zip (
+        sorted(qos_overrides.items(), key=lambda x: x[0]), expected_params
+    ):
+        assert actual[0] == expected[0]  # same param name
+        assert actual[1].value == expected[1]  # same param value
+
     rclpy.shutdown()
