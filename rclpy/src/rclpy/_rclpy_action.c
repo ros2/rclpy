57427a8c8c38f85fdffe21ece1d5f9566271889e
Chris Lalancette <clalancette@openrobotics.org>
2021-02-01 19:01:03 +0000

    Use copy-assignment for original_error.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rclpy/src/rclpy/_rclpy_action.c
+++ rclpy/src/rclpy/_rclpy_action.c
@@ -1576,13 +1576,7 @@ rclpy_action_process_cancel_request(PyObject * Py_UNUSED(self), PyObject * args)
     action_server, cancel_request, &cancel_response);
   destroy_cancel_request(cancel_request);
   if (RCL_RET_OK != ret) {
-    rcl_allocator_t allocator = rcl_get_default_allocator();
-    bool allocated_error = true;
-    char * original_error = rcutils_strdup(rcl_get_error_string().str, allocator);
-    if (original_error == NULL) {
-      original_error = "Failed to allocate memory for error";
-      allocated_error = false;
-    }
+    rcutils_error_string_t original_error = rcl_get_error_string();
     const char * extra_error = "";
     ret = rcl_action_cancel_response_fini(&cancel_response);
     if (RCL_RET_OK != ret) {
@@ -1591,10 +1585,7 @@ rclpy_action_process_cancel_request(PyObject * Py_UNUSED(self), PyObject * args)
     PyErr_Format(
       PyExc_RuntimeError,
       "Failed to process cancel request: %s%s",
-      original_error, extra_error);
-    if (allocated_error) {
-      allocator.deallocate(original_error, allocator.state);
-    }
+      original_error.str, extra_error);
     rcl_reset_error();
     return NULL;
   }
