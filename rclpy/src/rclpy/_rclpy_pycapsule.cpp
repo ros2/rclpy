efc9b6d82c1497e1bf7cd26ca3833f81fc7a31d4
Shane Loretz <sloretz@osrfoundation.org>
2021-02-23 11:38:48 -0800

    Don't include Python.h if including pybind11.h
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/src/rclpy/_rclpy_pycapsule.cpp
+++ rclpy/src/rclpy/_rclpy_pycapsule.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <Python.h>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
