3e030c09a3fbb1a5c4ac1f7d827c1deffff5d242
Shane Loretz <sloretz@openrobotics.org>
2021-02-26 10:28:49 -0800

    Use pybind11 for _rclpy_handle (#668)
    * _rclpy_handle.c -> .cpp
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * _rclpy_handle.c -> .cpp in CMake + no rcutils dep
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Use Pybind11 for Handle
    
    Also removes redunant API rclpy_handle_get_name() and
    replaced it with an existing duplicate API rclpy_pycapsule_name().
    
    Signed-off-by: Shane Loretz <sloretz@openrobotics.org>
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Do define Py_DEBUG on Windows
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Remove redundant Python.h include
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * static_assert that size_t can hold void *
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * static_assert needs a message
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

---
+++ rclpy/src/rclpy/_rclpy_handle.cpp
@@ -0,0 +1,37 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+#include "rclpy_common/handle.h"
+
+size_t
+rclpy_handle_get_pointer(py::capsule handle_capsule)
+{
+  void * ptr = rclpy_handle_get_pointer_from_capsule(handle_capsule.ptr(), handle_capsule.name());
+
+  if (!ptr) {
+    throw py::error_already_set();
+  }
+
+  static_assert(sizeof(size_t) >= sizeof(void *), "Need size_t capable of holding void *");
+  return reinterpret_cast<size_t>(ptr);
+}
+
+PYBIND11_MODULE(_rclpy_handle, m) {
+  m.doc() = "rclpy module for working with Handle objects.";
+  m.def("rclpy_handle_get_pointer", &rclpy_handle_get_pointer, "Get handle pointer.");
+}
