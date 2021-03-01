7b9e9f75249f68e5f51415e81f941569737c358b
Shane Loretz <sloretz@osrfoundation.org>
2021-03-01 13:25:21 -0800

    Include Pybind11 before Python.h
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/src/rclpy/context.cpp
+++ rclpy/src/rclpy/context.cpp
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Include pybind11 before rclpy_common/handle.h includes Python.h
+#include <pybind11/pybind11.h>
+
 #include <rcl/context.h>
 #include <rcl/types.h>
 
