77c4fb3f2f035d09181d1faa4ce612a5190d1ba7
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2021-04-14 20:44:42 +0200

    Convert WaitSet to use C++ Classes (#769)
    * Convert WaitSet to use C++ Classes
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * added feedback
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- rclpy/src/rclpy/action_server.cpp
+++ rclpy/src/rclpy/action_server.cpp
@@ -248,15 +248,14 @@ ActionServer::get_num_entities()
 }
 
 py::tuple
-ActionServer::is_ready(py::capsule pywait_set)
+ActionServer::is_ready(WaitSet & wait_set)
 {
-  auto wait_set = static_cast<rcl_wait_set_t *>(pywait_set);
   bool is_goal_request_ready = false;
   bool is_cancel_request_ready = false;
   bool is_result_request_ready = false;
   bool is_goal_expired = false;
   rcl_ret_t ret = rcl_action_server_wait_set_get_entities_ready(
-    wait_set,
+    wait_set.rcl_ptr(),
     rcl_action_server_.get(),
     &is_goal_request_ready,
     &is_cancel_request_ready,
@@ -276,10 +275,10 @@ ActionServer::is_ready(py::capsule pywait_set)
 }
 
 void
-ActionServer::add_to_waitset(py::capsule pywait_set)
+ActionServer::add_to_waitset(WaitSet & wait_set)
 {
-  auto wait_set = static_cast<rcl_wait_set_t *>(pywait_set);
-  rcl_ret_t ret = rcl_action_wait_set_add_action_server(wait_set, rcl_action_server_.get(), NULL);
+  rcl_ret_t ret = rcl_action_wait_set_add_action_server(
+    wait_set.rcl_ptr(), rcl_action_server_.get(), NULL);
   if (RCL_RET_OK != ret) {
     throw rclpy::RCLError("Failed to add 'rcl_action_server_t' to wait set");
   }
