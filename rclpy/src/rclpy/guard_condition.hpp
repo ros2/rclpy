4861d46652133f82309cd387bb1f5e9301d53243
Shane Loretz <sloretz@osrfoundation.org>
2021-04-28 13:54:05 -0700

    Remove rclpy::GuardCondition.pycapsule()
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/src/rclpy/guard_condition.hpp
+++ rclpy/src/rclpy/guard_condition.hpp
@@ -55,19 +55,6 @@ public:
     return rcl_guard_condition_.get();
   }
 
-  // TODO(ahcorde): Remove the pycapsule method when #728 is in
-  /// Return a pycapsule object to be able to handle the signal in C.
-  py::capsule
-  pycapsule() const
-  {
-    PyObject * capsule = rclpy_create_handle_capsule(
-      rcl_guard_condition_.get(), "rcl_guard_condition_t", _rclpy_destroy_guard_condition);
-    if (!capsule) {
-      throw py::error_already_set();
-    }
-    return py::reinterpret_steal<py::capsule>(capsule);
-  }
-
   /// Force an early destruction of this object
   void destroy() override;
 
