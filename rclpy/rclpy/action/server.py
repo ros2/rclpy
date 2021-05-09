503585169e2245631b50404b5da796f8dfda24fa
Tomoya Fujita <Tomoya.Fujita@sony.com>
2021-05-09 21:21:37 +0900

    reject cancel request if failed to transit to CANCEL_GOAL state.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rclpy/rclpy/action/server.py
+++ rclpy/rclpy/action/server.py
@@ -375,7 +375,15 @@ class ActionServer(Waitable):
 
             if CancelResponse.ACCEPT == response:
                 # Notify goal handle
-                goal_handle._update_state(GoalEvent.CANCEL_GOAL)
+                try:
+                    # If the goal's just scceeded after user cancel callback
+                    # that will generate exception from invalid transition.
+                    goal_handle._update_state(GoalEvent.CANCEL_GOAL)
+                except Exception as ex:
+                    self._node.get_logger().error(
+                        'Failed to cancel goal in cancel callback: {0}'.format(ex))
+                    # Remove from response since goal has been succeeded
+                    cancel_response.goals_canceling.remove(goal_info)
             else:
                 # Remove from response
                 cancel_response.goals_canceling.remove(goal_info)
