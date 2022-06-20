d6dd1911359ac8cd363716c996d241fe771b2913
Brian <brian.chen@openrobotics.org>
2022-06-20 13:15:06 -0400

    Update rclpy/rclpy/parameter.py
    Co-authored-by: Jacob Perron <jacob@openrobotics.org>
    Signed-off-by: Brian Chen <brian.chen@openrobotics.org>

--- rclpy/rclpy/parameter.py
+++ rclpy/rclpy/parameter.py
@@ -309,8 +309,7 @@ def parameter_dict_from_yaml_file(
         for n in param_keys:
             value = param_file[n]
             if type(value) != dict or 'ros__parameters' not in value:
-                raise RuntimeError( 'YAML file is not a valid ROS parameter file for node {}'
-                                   .format(n))
+                raise RuntimeError( f'YAML file is not a valid ROS parameter file for node {n}')
             param_dict.update(value['ros__parameters'])
         return _unpack_parameter_dict(namespace, param_dict)
 
