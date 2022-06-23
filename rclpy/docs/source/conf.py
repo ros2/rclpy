2139870887d0438e308606687c912b66e69482ef
Brian Chen <brian.chen@openrobotics.org>
2022-06-23 11:10:15 -0700

    fix linting
    Signed-off-by: Brian Chen <brian.chen@openrobotics.org>

--- rclpy/docs/source/conf.py
+++ rclpy/docs/source/conf.py
@@ -32,7 +32,7 @@
 
 
 # -- Project information -----------------------------------------------------
-import sphinx_rtd_theme
+import sphinx_rtd_theme  # noqa : F401
 
 project = 'rclpy'
 copyright = '2016-2022, Open Source Robotics Foundation, Inc.'  # noqa
