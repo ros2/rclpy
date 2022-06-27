e3b683e8ab2ac18523c41633df92c28894d23434
Brian <brian.chen@openrobotics.org>
2022-06-27 15:32:34 -0700

    Change sphinx theme to readthedocs (#950)

--- rclpy/docs/source/conf.py
+++ rclpy/docs/source/conf.py
@@ -58,6 +58,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -93,7 +94,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
