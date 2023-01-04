2d06dd2806cb987ddf8fd2b8843d5ac4dd210ca3
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2023-01-04 12:02:22 -0300

    Add default preset qos profile (#1062)
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
    
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rclpy/rclpy/qos.py
+++ rclpy/rclpy/qos.py
@@ -443,6 +443,8 @@ LivelinessLeaseDurationeBestAvailable = Duration(
 #: Used for initialization. Should not be used as the actual QoS profile.
 qos_profile_unknown = QoSProfile(**_rclpy.rmw_qos_profile_t.predefined(
     'qos_profile_unknown').to_dict())
+qos_profile_default = QoSProfile(**_rclpy.rmw_qos_profile_t.predefined(
+    'qos_profile_default').to_dict())
 #: Uses the default QoS settings defined in the DDS vendor tool
 qos_profile_system_default = QoSProfile(**_rclpy.rmw_qos_profile_t.predefined(
     'qos_profile_system_default').to_dict())
@@ -480,6 +482,7 @@ qos_profile_action_status_default = QoSProfile(**_rclpy.rclpy_action_get_rmw_qos
 
 class QoSPresetProfiles(Enum):
     UNKNOWN = qos_profile_unknown
+    DEFAULT = qos_profile_default
     SYSTEM_DEFAULT = qos_profile_system_default
     SENSOR_DATA = qos_profile_sensor_data
     SERVICES_DEFAULT = qos_profile_services_default
