1c147ee6143971da47c4f0ce0ec583fffd831407
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2023-04-12 14:08:50 -0700

    Stub type hash value line in TopicEndpointInfo string (#1110)
    * Stub type hash value line in TopicEndpointInfo string
    
    Part of ros2/ros2#1159
    
    For use in backported ros2cli extension for first Iron-sync (post-release). Adding now for any parsers that expect constant format of this output
    
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rclpy/test/test_topic_endpoint_info.py
+++ rclpy/test/test_topic_endpoint_info.py
@@ -93,6 +93,7 @@ class TestQosProfile(unittest.TestCase):
         expected_info_str = 'Node name: \n' \
             'Node namespace: \n' \
             'Topic type: \n' \
+            'Topic type hash: UNKNOWN\n' \
             'Endpoint type: INVALID\n' \
             'GID: \n' \
             'QoS profile:\n' \
