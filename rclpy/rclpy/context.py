26817ec0d8dc06880678b407f8690f367c5b92e6
Felix Divo <4403130+felixdivo@users.noreply.github.com>
2023-05-02 17:29:52 +0200

    Turn Context into a ContextManager (#1117)
    * Turn Context into a ContextManager
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    * Add a test for Context as ContextManager
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    * Update rclpy/rclpy/context.py
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    * Fiy typing of Context.__exit__
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    * Remove unused import
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    * Adhere to linter rules
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    * Add an example for using Context as a ContextManager to the docs
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Felix Divo <4403130+felixdivo@users.noreply.github.com>

--- rclpy/rclpy/context.py
+++ rclpy/rclpy/context.py
@@ -15,9 +15,12 @@
 from inspect import ismethod
 import sys
 import threading
+from types import TracebackType
 from typing import Callable
+from typing import ContextManager
 from typing import List
 from typing import Optional
+from typing import Type
 import weakref
 
 
@@ -25,12 +28,19 @@ g_logging_configure_lock = threading.Lock()
 g_logging_ref_count = 0
 
 
-class Context:
+class Context(ContextManager['Context']):
     """
     Encapsulates the lifecycle of init and shutdown.
 
     Context objects should not be reused, and are finalized in their destructor.
     Wraps the `rcl_context_t` type.
+
+    :Example:
+        >>> from rclpy.context import Context
+        >>> with Context() as context:
+        >>>     context.ok()
+        True
+
     """
 
     def __init__(self):
@@ -149,3 +159,17 @@ class Context:
             raise RuntimeError('Context must be initialized before it can have a domain id')
         with self.__context, self._lock:
             return self.__context.get_domain_id()
+
+    def __enter__(self) -> 'Context':
+        # We do not accept parameters here. If one wants to customize the init() call,
+        # they would have to call it manaully and not use the ContextManager convenience
+        self.init()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        self.try_shutdown()
