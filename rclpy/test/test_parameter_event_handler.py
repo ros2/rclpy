8dab37acc7d5a47ca3b9119e963316b5619d42ff
Chris Lalancette <clalancette@gmail.com>
2023-10-03 20:28:10 +0000

    Flake8 fix.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- rclpy/test/test_parameter_event_handler.py
+++ rclpy/test/test_parameter_event_handler.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-
 from typing import Union
+import unittest
 
 import pytest
 
