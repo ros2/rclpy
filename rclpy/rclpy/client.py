e3a40c9a8fd710e22fcd556194fcb4b8a370ede9
KKSTB <kkmail.home@gmail.com>
2023-10-17 11:34:15 +0800

    Add timeout_sec input to Client.call() to fix issue https://github.com/ros2/rclpy/issues/1181
    Signed-off-by: KKSTB <kkmail.home@gmail.com>

--- rclpy/rclpy/client.py
+++ rclpy/rclpy/client.py
@@ -69,14 +69,14 @@ class Client:
 
         self._lock = threading.Lock()
 
-    def call(self, request: SrvTypeRequest) -> SrvTypeResponse:
+    def call(self, request: SrvTypeRequest, timeout_sec: Optional[float] = None) -> SrvTypeResponse:
         """
         Make a service request and wait for the result.
 
-        .. warning:: Do not call this method in a callback or a deadlock may occur.
+        .. warning:: Do not call this method in a callback, or a deadlock or timeout may occur.
 
         :param request: The service request.
-        :return: The service response.
+        :return: The service response, or None if timed out.
         :raises: TypeError if the type of the passed request isn't an instance
           of the Request type of the provided service when the client was
           constructed.
@@ -97,7 +97,8 @@ class Client:
         # The callback might have been added after the future is completed,
         # resulting in the event never being set.
         if not future.done():
-            event.wait()
+            if not event.wait(timeout_sec):
+                self.remove_pending_request(future)
         if future.exception() is not None:
             raise future.exception()
         return future.result()
