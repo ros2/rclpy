e79106a448d6cbc1f92da4ee2c48ecc994f0e5e1
Chen Lihui <lihui.chen@sony.com>
2023-11-14 12:26:52 +0800

    single quotes preferred
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- rclpy/test/test_rosout_subscription.py
+++ rclpy/test/test_rosout_subscription.py
@@ -115,12 +115,12 @@ class TestRosoutSubscription(unittest.TestCase):
         self.assertTrue(self.fut.done())
 
     def test_logger_rosout_disabled_without_exception(self):
-        node = rclpy.create_node("mynode", context=self.context, enable_rosout=False)
+        node = rclpy.create_node('mynode', context=self.context, enable_rosout=False)
         try:
             logger = node.get_logger().get_child('child')
             logger.info('test')
         except Exception as e:
-            self.fail(f"Not expected failure: {e}")
+            self.fail(f'Not expected failure: {e}')
 
 
 if __name__ == '__main__':
