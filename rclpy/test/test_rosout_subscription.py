f90927818e2f84de4dbcd0a0e3ebe3cd744e81d3
Chen Lihui <lihui.chen@sony.com>
2023-11-15 23:44:08 +0800

    aligh with rcl that a rosout publisher of a node might not exist (#1196)
    * aligh with rcl that a rosout publisher of a node might not exist
    
    * check for RCL_RET_NOT_FOUND
    
    * print detailed message for exception
    
    reset error message for RCL_RET_NOT_FOUND as well
    
    * test for a node with rosout disabled
    
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- rclpy/test/test_rosout_subscription.py
+++ rclpy/test/test_rosout_subscription.py
@@ -114,12 +114,13 @@ class TestRosoutSubscription(unittest.TestCase):
         self.executor.spin_until_future_complete(self.fut, 3)
         self.assertTrue(self.fut.done())
 
-    def test_node_logger_not_exist(self):
-        node = rclpy.create_node('test_extra_node', context=self.context)
-        logger = node.get_logger()
-        node = None
-        with self.assertRaises(RuntimeError):
-            logger.get_child('child')
+    def test_logger_rosout_disabled_without_exception(self):
+        node = rclpy.create_node('mynode', context=self.context, enable_rosout=False)
+        try:
+            logger = node.get_logger().get_child('child')
+            logger.info('test')
+        except Exception as e:
+            self.fail(f'Not expected failure: {e}')
 
 
 if __name__ == '__main__':
