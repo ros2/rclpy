0dcf0789e2cd69d53447e58aa72c1040f1ddec4b
Tim Clephas <tim.clephas@nobleo.nl>
2024-02-13 19:02:24 +0100

    Allow action servers without execute callback
    Signed-off-by: Tim Clephas <tim.clephas@nobleo.nl>

--- rclpy/rclpy/action/server.py
+++ rclpy/rclpy/action/server.py
@@ -146,8 +146,20 @@ class ServerGoalHandle:
             # Publish
             self._action_server._handle.publish_feedback(feedback_message)
 
-    def succeed(self):
+    def executing(self):
+        self._update_state(_rclpy.GoalEvent.EXECUTE)
+
+    def succeed(self, response=None):
         self._update_state(_rclpy.GoalEvent.SUCCEED)
+
+        # Set result
+        result_response = self._action_server._action_type.Impl.GetResultService.Response()
+        result_response.status = self.status
+        if response is not None:
+            result_response.result = response
+        else:
+            result_response.result = self._action_server._action_type.Result()
+        self._action_server._result_futures[bytes(self.goal_id.uuid)].set_result(result_response)
 
     def abort(self):
         self._update_state(_rclpy.GoalEvent.ABORT)
@@ -186,7 +198,7 @@ class ActionServer(Waitable):
         node,
         action_type,
         action_name,
-        execute_callback,
+        execute_callback=None,
         *,
         callback_group=None,
         goal_callback=default_goal_callback,
@@ -233,7 +245,10 @@ class ActionServer(Waitable):
         self.register_handle_accepted_callback(handle_accepted_callback)
         self.register_goal_callback(goal_callback)
         self.register_cancel_callback(cancel_callback)
-        self.register_execute_callback(execute_callback)
+        if execute_callback:
+            self.register_execute_callback(execute_callback)
+        elif handle_accepted_callback is default_handle_accepted_callback:
+            self._logger.warning("Not handling nor executing the goal, this server will do nothing")
 
         # Import the typesupport for the action module if not already done
         check_for_type_support(action_type)
