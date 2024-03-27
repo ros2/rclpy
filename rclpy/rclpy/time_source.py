663defcb47ab1d13ca6e4d5064a2b24b19b8bebc
Michael Carlstrom <rmc@carlstrom.com>
2024-03-26 20:19:16 -0400

    Fix lint
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- rclpy/rclpy/time_source.py
+++ rclpy/rclpy/time_source.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List, Optional, Set, TYPE_CHECKING
 import weakref
-from typing import Optional, TYPE_CHECKING, List, Set
 
 from rcl_interfaces.msg import SetParametersResult
 from rclpy.clock import ROSClock
