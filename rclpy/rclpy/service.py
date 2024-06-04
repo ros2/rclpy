b49ceff714fabeb1a6b2a4c3cb963271bab1f52a
Chris Lalancette <clalancette@gmail.com>
2024-06-04 15:34:05 -0400

    Make service servers context-manager aware. (#1294)
    This way it is much easier to create examples that properly
    clean up after themselves.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- rclpy/rclpy/service.py
+++ rclpy/rclpy/service.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from types import TracebackType
 from typing import Callable
+from typing import Optional
+from typing import Type
 from typing import TypeVar
 
 from rclpy.callback_groups import CallbackGroup
@@ -115,3 +118,14 @@ class Service:
            should call :meth:`.Node.destroy_service`.
         """
         self.__service.destroy_when_not_in_use()
+
+    def __enter__(self) -> 'Service':
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        self.destroy()
