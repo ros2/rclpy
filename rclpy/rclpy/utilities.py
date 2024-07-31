7b7c4be1aab956bbf82d98ab202694320d4a5659
Michael Carlstrom <rmc@carlstrom.com>
2024-07-31 16:45:32 -0400

    Rerun CI
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- rclpy/rclpy/utilities.py
+++ rclpy/rclpy/utilities.py
@@ -70,6 +70,7 @@ def ok(*, context: Optional[Context] = None) -> bool:
     return context.ok()
 
 
+
 def shutdown(*, context: Optional[Context] = None) -> None:
     """
     Shutdown the given ``Context``.
