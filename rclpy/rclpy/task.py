cd1385ff6faebea1c33f0a4ada0b852452f45c4b
Nadav Elkabets <32939935+nadavelkabets@users.noreply.github.com>
2026-04-18 21:04:35 +0300

    Bugfix: executor doesn't propagate exception from task that awaited a future (#1643)
    * Schedule the original task when task awaits a future
    
    Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>
    
    * Add MultiThreadedExecutor to test
    
    Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>
    
    * Add tests for awaiting a done future and task cancellation during await
    
    Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>
    
    * Removed unused variable
    
    Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>
    
    ---------
    
    Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>
    (cherry picked from commit aac0ebb424a3045b20b52d6e62575f3af2fabfc8)
    
    # Conflicts:
    #	rclpy/rclpy/task.py
    #	rclpy/test/test_executor.py

--- rclpy/rclpy/task.py
+++ rclpy/rclpy/task.py
@@ -44,8 +44,13 @@ class Future:
         # An exception raised by the handler when called
         self._exception = None
         self._exception_fetched = False
+<<<<<<< HEAD
         # callbacks to be scheduled after this task completes
         self._callbacks = []
+=======
+        # callbacks or tasks to be scheduled after this task completes
+        self._callbacks: List[Union[Callable[['Future[T]'], None], 'Task[Any]']] = []
+>>>>>>> aac0ebb (Bugfix: executor doesn't propagate exception from task that awaited a future (#1643))
         # Lock for threadsafety
         self._lock = threading.Lock()
         # An executor to use when scheduling done callbacks
@@ -157,10 +162,18 @@ class Future:
         if executor is not None:
             # Have the executor take care of the callbacks
             for callback in callbacks:
-                executor.create_task(callback, self)
+                if isinstance(callback, Task):
+                    executor._call_task_in_next_spin(callback)
+                else:
+                    executor.create_task(callback, self)
         else:
             # No executor, call right away
             for callback in callbacks:
+                if isinstance(callback, Task):
+                    warnings.warn(
+                        'Dropping task awaiting future: '
+                        'executor reference could not be resolved')
+                    continue
                 try:
                     callback(self)
                 except Exception as e:
@@ -201,7 +214,26 @@ class Future:
         if invoke:
             callback(self)
 
+<<<<<<< HEAD
     def remove_done_callback(self, callback: Callable[['Future'], None]) -> bool:
+=======
+    def _add_waiting_task(self, task: 'Task[Any]') -> None:
+        """Schedule a task to resume when this future completes."""
+        with self._lock:
+            if not self._pending():
+                assert self._executor is not None
+                executor = self._executor()
+                if executor is not None:
+                    executor._call_task_in_next_spin(task)
+                else:
+                    warnings.warn(
+                        'Dropping task awaiting future: '
+                        'executor reference could not be resolved')
+            else:
+                self._callbacks.append(task)
+
+    def remove_done_callback(self, callback: Callable[['Future[T]'], None]) -> bool:
+>>>>>>> aac0ebb (Bugfix: executor doesn't propagate exception from task that awaited a future (#1643))
         """
         Remove a previously-added done callback.
 
@@ -317,9 +349,8 @@ class Task(Future):
         elif future_executor is not executor:
             raise RuntimeError('A task can only await futures associated with the same executor')
 
-        # The future is associated with the same executor, so we can resume the task directly
-        # in the done callback
-        future.add_done_callback(lambda _: self.__call__())
+        # Register the task to resume when the future is done or cancelled
+        future._add_waiting_task(self)
 
     def _complete_task(self) -> None:
         """Cleanup after task finished."""
