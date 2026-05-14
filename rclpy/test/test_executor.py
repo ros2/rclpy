88f8df6a46192a4fde37627a2460d6313ed45402
Chris Lalancette <clalancette@gmail.com>
2026-05-14 12:45:04 +0000

    Fix the flaky immediate shutdown.
    The test was trying to ensure that when shutdown is
    called, it happens "immediately", and not wait until
    the timeout.  The problem in our current code is that
    we don't know whether another thread is in "spin"
    during the shutdown.  If it is, then there is some possibility
    that we deleted the shutdown guard condition *before* the
    spin built the waitset, in which case we would spin
    until the timeout, and thus fail the test.
    
    The fix is to mark when a thread is in spin, and then
    have shutdown wait for the spin to complete before
    destroying resources.  If spin is waiting, then it will be
    woken by the guard condition immediately, and the shutdown
    will proceed quickly.  If the spin is stuck in a long-running
    user callback, then we still need a timeout because
    we don't want shutdown to take forever.
    
    While I was in here, I also enabled this test for the
    MultiThreadedExecutor since it should succeed for that now
    as well.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- rclpy/test/test_executor.py
+++ rclpy/test/test_executor.py
@@ -80,10 +80,9 @@ class TestExecutor(unittest.TestCase):
                 finally:
                     executor.shutdown()
 
-    @unittest.skip('Flaky on CI - see issue #1648')
     def test_executor_immediate_shutdown(self) -> None:
         self.assertIsNotNone(self.node.handle)
-        for cls in [SingleThreadedExecutor, EventsExecutor]:
+        for cls in [SingleThreadedExecutor, MultiThreadedExecutor, EventsExecutor]:
             with self.subTest(cls=cls):
                 executor = cls(context=self.context)
                 try:
