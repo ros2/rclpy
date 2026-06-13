5be7f35aed38e5c86b7155e5fb344fd4a424b2df
Michael Carlstrom <rmc@carlstrom.com>
2026-06-13 12:52:55 -0700

    Fix TypeIs import
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- rclpy/rclpy/experimental/async_timer.py
+++ rclpy/rclpy/experimental/async_timer.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import asyncio
 import inspect
 from typing import Callable, Optional
--- rclpy/rclpy/subscription.py
+++ rclpy/rclpy/subscription.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
 
 from enum import Enum
 import inspect
