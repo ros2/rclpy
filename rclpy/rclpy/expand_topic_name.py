f2db2c015184b52c0be72e315e8db308b1ba9ccc
Tomoya.Fujita <fujita.tomoya@triorb.co.jp>
2026-07-03 15:46:18 +0900

    resolve conflict.
    Signed-off-by: Tomoya.Fujita <fujita.tomoya@triorb.co.jp>

--- rclpy/rclpy/expand_topic_name.py
+++ rclpy/rclpy/expand_topic_name.py
@@ -24,15 +24,9 @@ def expand_topic_name(topic_name, node_name, node_namespace):
     The :py:func:validate_full_topic_name(): should be used on the expanded
     topic name to ensure it is valid after expansion.
 
-<<<<<<< HEAD
     :param topic_name str: topic name to be expanded
     :param node_name str: name of the node that this topic is associated with
-    :param namespace str: namespace that the topic is within
-=======
-    :param topic_name: topic name to be expanded
-    :param node_name: name of the node that this topic is associated with
-    :param node_namespace: namespace that the topic is within
->>>>>>> 93f95b7 (Fix incorrect parameter names in docstrings (#1683))
+    :param node_namespace str: namespace that the topic is within
     :returns: expanded topic name which is fully qualified
     :raises: ValueError if the topic name, node name or namespace are invalid
     """
