01c94215673f1f0b9afcee09b6fdf5e3af6a711f
Tomoya Fujita <fujita.tomoya@triorb.co.jp>
2026-08-07 13:46:45 +0900

    introduce ActionEndpointInfo to support "ros2 action info (-v)". (#1697)
    * introduce ActionEndpointInfo to support "ros2 action info (-v)".
    
    Signed-off-by: Tomoya Fujita <tomoya.fujita825@gmail.com>
    
    * address Copilot review comments.
    
    Signed-off-by: Tomoya Fujita <tomoya.fujita825@gmail.com>
    
    * harden the test wait loop more reliable.
    
    Signed-off-by: Tomoya Fujita <fujita.tomoya@triorb.co.jp>
    
    ---------
    
    Signed-off-by: Tomoya Fujita <tomoya.fujita825@gmail.com>
    Signed-off-by: Tomoya Fujita <fujita.tomoya@triorb.co.jp>

--- rclpy/rclpy/node.py
+++ rclpy/rclpy/node.py
@@ -56,7 +56,7 @@ from rclpy.clock import BaseClock, Clock
 from rclpy.clock_type import ClockType
 from rclpy.constants import S_TO_NS
 from rclpy.context import Context
-from rclpy.endpoint_info import ServiceEndpointInfo, TopicEndpointInfo
+from rclpy.endpoint_info import ActionEndpointInfo, ServiceEndpointInfo, TopicEndpointInfo
 from rclpy.event_handler import PublisherEventCallbacks
 from rclpy.event_handler import SubscriptionEventCallbacks
 from rclpy.exceptions import InvalidHandle
@@ -1754,6 +1754,46 @@ class BaseNode(ABC):
             return self._count_clients_or_servers(
                 service_name, self.handle.get_count_services)
 
+    def _count_action_clients_or_servers(
+        self,
+        action_name: str,
+        func: Callable[[str], int]
+    ) -> int:
+        fq_action_name = expand_topic_name(action_name, self.get_name(), self.get_namespace())
+        validate_full_topic_name(fq_action_name)
+        with self.handle:
+            return func(fq_action_name)
+
+    def count_action_clients(self, action_name: str) -> int:
+        """
+        Return the number of action clients on a given action.
+
+        `action_name` may be a relative, private, or fully qualified action name.
+        A relative or private action is expanded using this node's namespace and name.
+        The queried action name is not remapped.
+
+        :param action_name: the action_name on which to count the number of action clients.
+        :return: the number of action clients on the action.
+        """
+        with self.handle:
+            return self._count_action_clients_or_servers(
+                action_name, self.handle.get_count_action_clients)
+
+    def count_action_servers(self, action_name: str) -> int:
+        """
+        Return the number of action servers on a given action.
+
+        `action_name` may be a relative, private, or fully qualified action name.
+        A relative or private action is expanded using this node's namespace and name.
+        The queried action name is not remapped.
+
+        :param action_name: the action_name on which to count the number of action servers.
+        :return: the number of action servers on the action.
+        """
+        with self.handle:
+            return self._count_action_clients_or_servers(
+                action_name, self.handle.get_count_action_servers)
+
     def _get_info_by_topic(
         self,
         topic_name: str,
@@ -1922,6 +1962,65 @@ class BaseNode(ABC):
             service_name,
             no_mangle,
             _rclpy.rclpy_get_servers_info_by_service)
+
+    def _get_info_by_action(
+        self,
+        action_name: str,
+        func: Callable[['_rclpy.Node', str], list['_rclpy._ActionEndpointInfoDict']]
+    ) -> List[ActionEndpointInfo]:
+        with self.handle:
+            fq_action_name = expand_topic_name(
+                action_name, self.get_name(), self.get_namespace())
+            validate_full_topic_name(fq_action_name)
+            info_dicts = func(self.handle, fq_action_name)
+            infos = [ActionEndpointInfo(**x) for x in info_dicts]
+            return infos
+
+    def get_action_clients_info_by_action(
+        self,
+        action_name: str
+    ) -> List[ActionEndpointInfo]:
+        """
+        Return a list of action clients on a given action.
+
+        The returned parameter is a list of ActionEndpointInfo objects, where each aggregates
+        the endpoint information of all the underlying entities of one action client, i.e. the
+        clients of the goal, cancel, and result services and the subscriptions on the feedback
+        and status topics.
+
+        ``action_name`` may be a relative, private, or fully qualified action name.
+        A relative or private action will be expanded using this node's namespace and name.
+        The queried ``action_name`` is not remapped.
+
+        :param action_name: The action_name on which to find the action clients.
+        :return: A list of ActionEndpointInfo for all the action clients on this action.
+        """
+        return self._get_info_by_action(
+            action_name,
+            _rclpy.rclpy_get_action_clients_info_by_action)
+
+    def get_action_servers_info_by_action(
+        self,
+        action_name: str
+    ) -> List[ActionEndpointInfo]:
+        """
+        Return a list of action servers on a given action.
+
+        The returned parameter is a list of ActionEndpointInfo objects, where each aggregates
+        the endpoint information of all the underlying entities of one action server, i.e. the
+        servers of the goal, cancel, and result services and the publishers on the feedback
+        and status topics.
+
+        ``action_name`` may be a relative, private, or fully qualified action name.
+        A relative or private action will be expanded using this node's namespace and name.
+        The queried ``action_name`` is not remapped.
+
+        :param action_name: The action_name on which to find the action servers.
+        :return: A list of ActionEndpointInfo for all the action servers on this action.
+        """
+        return self._get_info_by_action(
+            action_name,
+            _rclpy.rclpy_get_action_servers_info_by_action)
 
     def _create_publisher_handle(
         self,
