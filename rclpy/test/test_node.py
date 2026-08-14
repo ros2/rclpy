ros2/rclpy

commit 45b4bc165949674922becd27287035dce5e140b5

Feature: streamlined entity destroy (#1629)
Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>

rclpy/rclpy/node.py
@@ -50,7 +50,7 @@ from rcl_interfaces.srv import ListParameters
 from rclpy.callback_groups import CallbackGroup
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.callback_groups import ReentrantCallbackGroup
-from rclpy.client import Client
+from rclpy.client import BaseClient, Client
 from rclpy.clock import Clock
 from rclpy.clock_type import ClockType
 from rclpy.constants import S_TO_NS
@@ -85,6 +85,7 @@ from rclpy.qos import qos_profile_services_default
 from rclpy.qos import QoSProfile
 from rclpy.qos_overriding_options import _declare_qos_parameters
 from rclpy.qos_overriding_options import QoSOverridingOptions
+from rclpy.service import BaseService
 from rclpy.service import Service
 from rclpy.service import ServiceCallbackUnion
 from rclpy.subscription import GenericSubscriptionCallbackUnion
@@ -92,7 +93,7 @@ from rclpy.subscription import Subscription
 from rclpy.subscription import SubscriptionCallbackUnion
 from rclpy.subscription_content_filter_options import ContentFilterOptions
 from rclpy.time_source import TimeSource
-from rclpy.timer import Rate
+from rclpy.timer import BaseTimer, Rate
 from rclpy.timer import Timer
 from rclpy.timer import TimerCallbackUnion
 from rclpy.type_description_service import TypeDescriptionService
@@ -1622,6 +1623,7 @@ class Node:
         try:
             publisher = publisher_class(
                 publisher_object, msg_type, topic, qos_profile,
+                on_destroy=self._on_destroy_publisher,
                 event_callbacks=event_callbacks or PublisherEventCallbacks(),
                 callback_group=callback_group)
         except Exception:
@@ -1755,6 +1757,7 @@ class Node:
             subscription = Subscription(
                 subscription_object, msg_type,
                 topic, callback, qos_profile, raw,
+                on_destroy=self._on_destroy_subscription,
                 callback_group=callback_group,
                 event_callbacks=event_callbacks or SubscriptionEventCallbacks())
         except Exception:
@@ -1805,6 +1808,7 @@ class Node:
         client = Client(
             self.context,
             client_impl, srv_type, srv_name, qos_profile,
+            on_destroy=self._on_destroy_client,
             callback_group=callback_group)
         callback_group.add_entity(client)
         self._clients.append(client)
@@ -1850,6 +1854,7 @@ class Node:
         service = Service(
             service_impl,
             srv_type, srv_name, callback, qos_profile,
+            on_destroy=self._on_destroy_service,
             callback_group=callback_group)
         callback_group.add_entity(service)
         self._services.append(service)
@@ -1888,6 +1893,7 @@ class Node:
         timer = Timer(
             callback, timer_period_nsec, clock,
             callback_group=callback_group,
+            on_destroy=self._on_destroy_timer,
             context=self.context,
             autostart=autostart
         )
@@ -1941,6 +1947,30 @@ class Node:
         timer = self.create_timer(period, callback, group, clock)
         return Rate(timer, context=self.context)
 
+    def _on_destroy_publisher(self, publisher: Publisher) -> None:
+        self._publishers.remove(publisher)
+        for event_handler in publisher.event_handlers:
+            self.__waitables.remove(event_handler)
+        self._wake_executor()
+
+    def _on_destroy_subscription(self, subscription: Subscription[Any]) -> None:
+        self._subscriptions.remove(subscription)
+        for event_handler in subscription.event_handlers:
+            self.__waitables.remove(event_handler)
+        self._wake_executor()
+
+    def _on_destroy_client(self, client: BaseClient[Any, Any]) -> None:
+        self._clients.remove(client)
+        self._wake_executor()
+
+    def _on_destroy_service(self, service: BaseService[Any, Any]) -> None:
+        self._services.remove(service)
+        self._wake_executor()
+
+    def _on_destroy_timer(self, timer: BaseTimer) -> None:
+        self._timers.remove(timer)
+        self._wake_executor()
+
     def destroy_publisher(self, publisher: Publisher[Any]) -> bool:
         """
         Destroy a publisher created by the node.
@@ -1948,14 +1978,7 @@ class Node:
         :return: ``True`` if successful, ``False`` otherwise.
         """
         if publisher in self._publishers:
-            self._publishers.remove(publisher)
-            for event_handler in publisher.event_handlers:
-                self.__waitables.remove(event_handler)
-            try:
-                publisher.destroy()
-            except InvalidHandle:
-                return False
-            self._wake_executor()
+            publisher.destroy()
             return True
         return False
 
@@ -1966,14 +1989,7 @@ class Node:
         :return: ``True`` if successful, ``False`` otherwise.
         """
         if subscription in self._subscriptions:
-            self._subscriptions.remove(subscription)
-            for event_handler in subscription.event_handlers:
-                self.__waitables.remove(event_handler)
-            try:
-                subscription.destroy()
-            except InvalidHandle:
-                return False
-            self._wake_executor()
+            subscription.destroy()
             return True
         return False
 
@@ -1984,12 +2000,7 @@ class Node:
         :return: ``True`` if successful, ``False`` otherwise.
         """
         if client in self._clients:
-            self._clients.remove(client)
-            try:
-                client.destroy()
-            except InvalidHandle:
-                return False
-            self._wake_executor()
+            client.destroy()
             return True
         return False
 
@@ -2000,12 +2011,7 @@ class Node:
         :return: ``True`` if successful, ``False`` otherwise.
         """
         if service in self._services:
-            self._services.remove(service)
-            try:
-                service.destroy()
-            except InvalidHandle:
-                return False
-            self._wake_executor()
+            service.destroy()
             return True
         return False
 
@@ -2016,12 +2022,7 @@ class Node:
         :return: ``True`` if successful, ``False`` otherwise.
         """
         if timer in self._timers:
-            self._timers.remove(timer)
-            try:
-                timer.destroy()
-            except InvalidHandle:
-                return False
-            self._wake_executor()
+            timer.destroy()
             return True
         return False
 
@@ -2085,7 +2086,6 @@ class Node:
             self.destroy_timer(self._timers[0])
         while self._guards:
             self.destroy_guard_condition(self._guards[0])
-        self._type_description_service.destroy()
         self.__node.destroy_when_not_in_use()
         self._wake_executor()
 

rclpy/rclpy/type_description_service.py
@@ -77,12 +77,6 @@ class TypeDescriptionService:
         if self.enabled:
             self._start_service()
 
-    def destroy(self) -> None:
-        # Required manual destruction because this is not managed by rclpy.Service
-        if self._type_description_srv is not None:
-            self._type_description_srv.destroy_when_not_in_use()
-            self._type_description_srv = None
-
     def _start_service(self) -> None:
         node = self._get_node()
         self._type_description_srv = _rclpy.TypeDescriptionService(node.handle)
@@ -94,6 +88,7 @@ class TypeDescriptionService:
             srv_type=GetTypeDescription,
             srv_name=self.service_name,
             callback=self._service_callback,
+            on_destroy=node._on_destroy_service,
             callback_group=node.default_callback_group,
             qos_profile=qos_profile_services_default)
         node.default_callback_group.add_entity(service)

rclpy/rclpy/lifecycle/node.py
@@ -144,6 +144,7 @@ class LifecycleNodeMixin(ManagedEntity):
                 self._state_machine.service_change_state.name,
                 self.__on_change_state,
                 QoSProfile(**self._state_machine.service_change_state.qos),
+                on_destroy=self._on_destroy_service,
                 callback_group=callback_group)
             self._service_get_state = Service(
                 self._state_machine.service_get_state,
@@ -151,6 +152,7 @@ class LifecycleNodeMixin(ManagedEntity):
                 self._state_machine.service_get_state.name,
                 self.__on_get_state,
                 QoSProfile(**self._state_machine.service_get_state.qos),
+                on_destroy=self._on_destroy_service,
                 callback_group=callback_group)
             self._service_get_available_states = Service(
                 self._state_machine.service_get_available_states,
@@ -158,6 +160,7 @@ class LifecycleNodeMixin(ManagedEntity):
                 self._state_machine.service_get_available_states.name,
                 self.__on_get_available_states,
                 QoSProfile(**self._state_machine.service_get_available_states.qos),
+                on_destroy=self._on_destroy_service,
                 callback_group=callback_group)
             self._service_get_available_transitions = Service(
                 self._state_machine.service_get_available_transitions,
@@ -165,6 +168,7 @@ class LifecycleNodeMixin(ManagedEntity):
                 self._state_machine.service_get_available_transitions.name,
                 self.__on_get_available_transitions,
                 QoSProfile(**self._state_machine.service_get_available_transitions.qos),
+                on_destroy=self._on_destroy_service,
                 callback_group=callback_group)
             self._service_get_transition_graph = Service(
                 self._state_machine.service_get_transition_graph,
@@ -172,6 +176,7 @@ class LifecycleNodeMixin(ManagedEntity):
                 self._state_machine.service_get_transition_graph.name,
                 self.__on_get_transition_graph,
                 QoSProfile(**self._state_machine.service_get_transition_graph.qos),
+                on_destroy=self._on_destroy_service,
                 callback_group=callback_group)
 
             lifecycle_services = [

rclpy/rclpy/client.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections.abc import Callable
 import threading
 import time
 from types import TracebackType
@@ -46,12 +47,16 @@ class BaseClient(Generic[SrvRequestT, SrvResponseT]):
         srv_type: type[Srv[SrvRequestT, SrvResponseT]],
         srv_name: str,
         qos_profile: QoSProfile,
+        *,
+        on_destroy: Optional[Callable[['BaseClient[SrvRequestT, SrvResponseT]'], None]] = None,
     ) -> None:
         self.context = context
         self.__client = client_impl
         self.srv_type = srv_type
         self.srv_name = srv_name
         self.qos_profile = qos_profile
+        self._on_destroy = on_destroy
+        self._destroyed = False
 
     def service_is_ready(self) -> bool:
         """
@@ -95,13 +100,17 @@ class BaseClient(Generic[SrvRequestT, SrvResponseT]):
             return self.__client.get_logger_name()
 
     def destroy(self) -> None:
-        """
-        Destroy a container for a ROS service client.
+        """Destroy the client, notifying the owning node and releasing the handle."""
+        if self._destroyed:
+            return
+        self._destroyed = True
+        if self._on_destroy is not None:
+            self._on_destroy(self)
+            self._on_destroy = None
+        self._destroy()
 
-        .. warning:: Users should not destroy a service client with this destructor, instead they
-           should call :meth:`.Node.destroy_client`.
-        """
-        self.__client.destroy_when_not_in_use()
+    def _destroy(self) -> None:
+        self.handle.destroy_when_not_in_use()
 
     def __enter__(self) -> Self:
         return self
@@ -124,6 +133,7 @@ class Client(BaseClient[SrvRequestT, SrvResponseT], Generic[SrvRequestT, SrvResp
         srv_name: str,
         qos_profile: QoSProfile,
         *,
+        on_destroy: Optional[Callable[['BaseClient[SrvRequestT, SrvResponseT]'], None]] = None,
         callback_group: CallbackGroup
     ) -> None:
         """
@@ -145,7 +155,8 @@ class Client(BaseClient[SrvRequestT, SrvResponseT], Generic[SrvRequestT, SrvResp
             client_impl=client_impl,
             srv_type=srv_type,
             srv_name=srv_name,
-            qos_profile=qos_profile
+            qos_profile=qos_profile,
+            on_destroy=on_destroy
         )
         # Key is a sequence number, value is an instance of a Future
         self._pending_requests: Dict[int, Future[SrvResponseT]] = {}

rclpy/rclpy/lifecycle/publisher.py
@@ -14,7 +14,8 @@
 
 from __future__ import annotations
 
-from typing import Generic, Tuple, Type, TYPE_CHECKING, TypedDict, Union
+from collections.abc import Callable
+from typing import Generic, Optional, Tuple, Type, TYPE_CHECKING, TypedDict, Union
 
 from rclpy.callback_groups import CallbackGroup
 from rclpy.event_handler import PublisherEventCallbacks
@@ -37,6 +38,7 @@ if TYPE_CHECKING:
         msg_type: Type[MsgT]
         topic: str
         qos_profile: QoSProfile
+        on_destroy: Optional[Callable[['Publisher[MsgT]'], None]]
         event_callbacks: PublisherEventCallbacks
         callback_group: CallbackGroup
 

rclpy/rclpy/publisher.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections.abc import Callable
 from types import TracebackType
 from typing import Generic, List, Optional, Type, TypeVar, Union
 
@@ -37,11 +38,15 @@ class BasePublisher(Generic[MsgT]):
         msg_type: Type[MsgT],
         topic: str,
         qos_profile: QoSProfile,
+        *,
+        on_destroy: Optional[Callable[['BasePublisher[MsgT]'], None]] = None,
     ) -> None:
         self.__publisher = publisher_impl
         self.msg_type = msg_type
         self.topic = topic
         self.qos_profile = qos_profile
+        self._on_destroy = on_destroy
+        self._destroyed = False
 
     def publish(self, msg: Union[MsgT, bytes]) -> None:
         """
@@ -80,7 +85,17 @@ class BasePublisher(Generic[MsgT]):
             return self.__publisher.get_logger_name()
 
     def destroy(self) -> None:
-        self.__publisher.destroy_when_not_in_use()
+        """Destroy the publisher, notifying the owning node and releasing the handle."""
+        if self._destroyed:
+            return
+        self._destroyed = True
+        if self._on_destroy is not None:
+            self._on_destroy(self)
+            self._on_destroy = None
+        self._destroy()
+
+    def _destroy(self) -> None:
+        self.handle.destroy_when_not_in_use()
 
     def assert_liveliness(self) -> None:
         """
@@ -113,6 +128,7 @@ class Publisher(BasePublisher[MsgT], Generic[MsgT]):
         topic: str,
         qos_profile: QoSProfile,
         *,
+        on_destroy: Optional[Callable[['Publisher[MsgT]'], None]] = None,
         event_callbacks: PublisherEventCallbacks,
         callback_group: CallbackGroup,
     ) -> None:
@@ -134,8 +150,9 @@ class Publisher(BasePublisher[MsgT], Generic[MsgT]):
             publisher_impl=publisher_impl,
             msg_type=msg_type,
             topic=topic,
-            qos_profile=qos_profile
+            qos_profile=qos_profile,
         )
+        self._on_destroy = on_destroy
 
         self.event_handlers: List[EventHandler] = event_callbacks.create_event_handlers(
             callback_group, publisher_impl, topic)
@@ -160,13 +177,7 @@ class Publisher(BasePublisher[MsgT], Generic[MsgT]):
         with self.handle:
             return self.handle.wait_for_all_acked(timeout._duration_handle)
 
-    def destroy(self) -> None:
-        """
-        Destroy a container for a ROS publisher.
-
-        .. warning:: Users should not destroy a publisher with this method, instead they should
-           call :meth:`.Node.destroy_publisher`.
-        """
+    def _destroy(self) -> None:
         for handler in self.event_handlers:
             handler.destroy()
-        super().destroy()
+        super()._destroy()

rclpy/rclpy/service.py
@@ -53,13 +53,17 @@ class BaseService(Generic[SrvRequestT, SrvResponseT]):
         srv_type: type[Srv[SrvRequestT, SrvResponseT]],
         srv_name: str,
         callback: ServiceCallbackUnion[SrvRequestT, SrvResponseT],
-        qos_profile: QoSProfile
+        qos_profile: QoSProfile,
+        *,
+        on_destroy: Optional[Callable[['BaseService[SrvRequestT, SrvResponseT]'], None]] = None,
     ) -> None:
         self.__service = service_impl
         self.srv_type = srv_type
         self.srv_name = srv_name
         self.callback = callback
         self.qos_profile = qos_profile
+        self._on_destroy = on_destroy
+        self._destroyed = False
 
     def _send_response(
         self,
@@ -109,13 +113,17 @@ class BaseService(Generic[SrvRequestT, SrvResponseT]):
             return self.__service.get_logger_name()
 
     def destroy(self) -> None:
-        """
-        Destroy a container for a ROS service server.
+        """Destroy the service, notifying the owning node and releasing the handle."""
+        if self._destroyed:
+            return
+        self._destroyed = True
+        if self._on_destroy is not None:
+            self._on_destroy(self)
+            self._on_destroy = None
+        self._destroy()
 
-        .. warning:: Users should not destroy a service server with this destructor, instead they
-           should call :meth:`.Node.destroy_service`.
-        """
-        self.__service.destroy_when_not_in_use()
+    def _destroy(self) -> None:
+        self.handle.destroy_when_not_in_use()
 
     def __enter__(self) -> Self:
         return self
@@ -138,6 +146,7 @@ class Service(BaseService[SrvRequestT, SrvResponseT], Generic[SrvRequestT, SrvRe
         callback: ServiceCallbackUnion[SrvRequestT, SrvResponseT],
         qos_profile: QoSProfile,
         *,
+        on_destroy: Optional[Callable[['Service[SrvRequestT, SrvResponseT]'], None]] = None,
         callback_group: CallbackGroup
     ) -> None:
         """
@@ -160,7 +169,8 @@ class Service(BaseService[SrvRequestT, SrvResponseT], Generic[SrvRequestT, SrvRe
             srv_type=srv_type,
             srv_name=srv_name,
             callback=callback,
-            qos_profile=qos_profile
+            qos_profile=qos_profile,
+            on_destroy=on_destroy
         )
         self.callback_group = callback_group
         # True when the callback is ready to fire but has not been "taken" by an executor

rclpy/rclpy/subscription.py
@@ -84,6 +84,8 @@ class BaseSubscription(Generic[MsgT]):
          callback: GenericSubscriptionCallbackUnion[bytes],
          qos_profile: QoSProfile,
          raw: Literal[True],
+         *,
+         on_destroy: Optional[Callable[['BaseSubscription[MsgT]'], None]] = None,
     ) -> None: ...
 
     @overload
@@ -95,6 +97,8 @@ class BaseSubscription(Generic[MsgT]):
          callback: GenericSubscriptionCallbackUnion[MsgT],
          qos_profile: QoSProfile,
          raw: Literal[False],
+         *,
+         on_destroy: Optional[Callable[['BaseSubscription[MsgT]'], None]] = None,
     ) -> None: ...
 
     @overload
@@ -106,6 +110,8 @@ class BaseSubscription(Generic[MsgT]):
          callback: SubscriptionCallbackUnion[MsgT],
          qos_profile: QoSProfile,
          raw: bool,
+         *,
+         on_destroy: Optional[Callable[['BaseSubscription[MsgT]'], None]] = None,
     ) -> None: ...
 
     def __init__(
@@ -116,6 +122,8 @@ class BaseSubscription(Generic[MsgT]):
          callback: SubscriptionCallbackUnion[MsgT],
          qos_profile: QoSProfile,
          raw: bool,
+         *,
+         on_destroy: Optional[Callable[['BaseSubscription[MsgT]'], None]] = None,
     ) -> None:
         self.__subscription = subscription_impl
         self.msg_type = msg_type
@@ -123,6 +131,8 @@ class BaseSubscription(Generic[MsgT]):
         self.callback = callback
         self.qos_profile = qos_profile
         self.raw = raw
+        self._on_destroy = on_destroy
+        self._destroyed = False
 
     def get_publisher_count(self) -> int:
         """Get the number of publishers that this subscription has."""
@@ -134,6 +144,16 @@ class BaseSubscription(Generic[MsgT]):
         return self.__subscription
 
     def destroy(self) -> None:
+        """Destroy the subscription, notifying the owning node and releasing the handle."""
+        if self._destroyed:
+            return
+        self._destroyed = True
+        if self._on_destroy is not None:
+            self._on_destroy(self)
+            self._on_destroy = None
+        self._destroy()
+
+    def _destroy(self) -> None:
         self.handle.destroy_when_not_in_use()
 
     @property
@@ -231,6 +251,7 @@ class Subscription(BaseSubscription[MsgT], Generic[MsgT]):
          qos_profile: QoSProfile,
          raw: Literal[True],
          *,
+         on_destroy: Optional[Callable[['Subscription[MsgT]'], None]] = None,
          callback_group: CallbackGroup,
          event_callbacks: SubscriptionEventCallbacks,
     ) -> None: ...
@@ -245,6 +266,7 @@ class Subscription(BaseSubscription[MsgT], Generic[MsgT]):
          qos_profile: QoSProfile,
          raw: Literal[False],
          *,
+         on_destroy: Optional[Callable[['Subscription[MsgT]'], None]] = None,
          callback_group: CallbackGroup,
          event_callbacks: SubscriptionEventCallbacks,
     ) -> None: ...
@@ -259,6 +281,7 @@ class Subscription(BaseSubscription[MsgT], Generic[MsgT]):
          qos_profile: QoSProfile,
          raw: bool,
          *,
+         on_destroy: Optional[Callable[['Subscription[MsgT]'], None]] = None,
          callback_group: CallbackGroup,
          event_callbacks: SubscriptionEventCallbacks,
     ) -> None: ...
@@ -272,6 +295,7 @@ class Subscription(BaseSubscription[MsgT], Generic[MsgT]):
          qos_profile: QoSProfile,
          raw: bool,
          *,
+         on_destroy: Optional[Callable[['Subscription[MsgT]'], None]] = None,
          callback_group: CallbackGroup,
          event_callbacks: SubscriptionEventCallbacks,
     ) -> None:
@@ -301,6 +325,7 @@ class Subscription(BaseSubscription[MsgT], Generic[MsgT]):
             qos_profile=qos_profile,
             raw=raw
         )
+        self._on_destroy = on_destroy
         self.callback_group = callback_group
         # True when the callback is ready to fire but has not been "taken" by an executor
         self._executor_event = False
@@ -308,13 +333,7 @@ class Subscription(BaseSubscription[MsgT], Generic[MsgT]):
         self.event_handlers = event_callbacks.create_event_handlers(
             callback_group, subscription_impl, topic)
 
-    def destroy(self) -> None:
-        """
-        Destroy a container for a ROS subscription.
-
-        .. warning:: Users should not destroy a subscription with this method, instead they
-           should call :meth:`.Node.destroy_subscription`.
-        """
+    def _destroy(self) -> None:
         for handler in self.event_handlers:
             handler.destroy()
-        super().destroy()
+        super()._destroy()

commit e735ff85495ff865e19bc866f6a55c8c3bcdc1c8

fix: regression that makes destroyed nodes linger forever
Introduced in https://github.com/ros2/rclpy/pull/1629, the TypeDescriptionService was left alive after destroy_node, which means the node would never actually disappear from the ros graph until process exit.

Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

rclpy/rclpy/node.py
@@ -2150,6 +2150,8 @@ class BaseNode(ABC):
     def destroy_node(self) -> None:
         self._context.untrack_node(self)
         self._parameter_event_publisher = None
+        # Not owned by self._services, so Node.destroy_node() doesn't destroy it
+        self._type_description_service.destroy()
         self.handle.destroy_when_not_in_use()
 
 

rclpy/rclpy/type_description_service.py
@@ -76,6 +76,13 @@ class TypeDescriptionService:
         if self.enabled:
             self._start_service()
 
+    def destroy(self) -> None:
+        # Required manual destruction because this is not managed by rclpy.Service.
+        # The underlying C++ holds a shared node handle, so leaving this alive keeps node alive.
+        if self._type_description_srv is not None:
+            self._type_description_srv.destroy_when_not_in_use()
+            self._type_description_srv = None
+
     def _start_service(self) -> None:
         node = self._get_node()
         self._type_description_srv = _rclpy.TypeDescriptionService(node.handle)

rclpy/test/test_node.py
@@ -74,6 +74,16 @@ TEST_NAMESPACE = '/my_ns'
 TEST_RESOURCES_DIR = pathlib.Path(__file__).resolve().parent / 'resources' / 'test_node'
 
 
+def _wait_until(predicate: Callable[[], bool], timeout: float = 10.0) -> bool:
+    """Poll ``predicate`` until it is true, returning whether it became true within ``timeout``."""
+    end = time.monotonic() + timeout
+    while time.monotonic() < end:
+        if predicate():
+            return True
+        time.sleep(0.05)
+    return predicate()
+
+
 class TestNodeAllowUndeclaredParameters(unittest.TestCase):
 
     if TYPE_CHECKING:
@@ -257,6 +267,26 @@ class TestNodeAllowUndeclaredParameters(unittest.TestCase):
     def test_node_names_and_namespaces_with_enclaves(self) -> None:
         # test that it doesn't raise
         self.node.get_node_names_and_namespaces_with_enclaves()
+
+    def test_destroyed_node_leaves_the_graph(self) -> None:
+        # destroy_node() must release every reference to the node handle so rcl_node_fini() runs.
+        # Any surviving reference leaves the node advertised in the graph until its process exits.
+        doomed = rclpy.create_node('doomed_node', context=self.context)
+
+        def in_graph() -> bool:
+            return any(
+                name == 'doomed_node'
+                for name, _ in self.node.get_node_names_and_namespaces()
+            )
+
+        self.assertTrue(
+            _wait_until(in_graph), 'node under test never appeared in the graph')
+
+        doomed.destroy_node()
+
+        self.assertTrue(
+            _wait_until(lambda: not in_graph()),
+            'destroyed node is still in the graph, so its handle was never finalized')
 
     def assert_qos_equal(self, expected_qos_profile: QoSProfile,
                          actual_qos_profile: QoSProfile, *, is_publisher: bool) -> None: